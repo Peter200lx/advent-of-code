149e9a0a01453dddc4259bc187c99ffc2f4b32b8
Peter Olson <peter.olson@gmail.com>
2017-12-20 01:19:01 -0800

    Day 17 solutions

---
+++ 2017/day17.py
@@ -0,0 +1,34 @@
+from typing import Tuple, List
+
+NUMBER_REPS_P1 = 2017
+NUMBER_REPS_P2 = 50000000
+
+EXAMPLE_DATA = 3
+DATA = 366
+
+
+def build_list(num_steps: int, num_reps: int) -> Tuple[int, List[int]]:
+    cur_loc = 0
+    spin_list = [0]
+    for i in range(1, num_reps + 1):
+        cur_loc += num_steps + 1
+        cur_loc %= len(spin_list)
+        spin_list.insert(cur_loc, i)
+    return cur_loc, spin_list
+
+
+def track_1st_loc(num_steps: int, num_reps: int) -> int:
+    cur_loc = 0
+    loc_1st_val = 0
+    for i in range(1, num_reps + 1):
+        cur_loc = ((num_steps + cur_loc) % i) + 1
+        if cur_loc is 1:
+            loc_1st_val = i
+    return loc_1st_val
+
+
+if __name__ == '__main__':
+    location, run_list = build_list(DATA, NUMBER_REPS_P1)
+    print(run_list[location-3:location+4])
+
+    print(track_1st_loc(DATA, NUMBER_REPS_P2))
