404127e74c78f74571e94623859075afb4094e26
Peter Olson <peter.olson@gmail.com>
2017-12-20 01:16:43 -0800

    Day 13 with part 2

--- 2017/day13.py
+++ 2017/day13.py
@@ -49,69 +49,40 @@ EXAMPLE_DATA = """0: 3
 
 class FirewallLayer(object):
 
-    def __init__(self, depth):
-        self.state = [False for _ in range(depth)]
-        if self.state:
-            self.state[0] = True
-        self.direction = 1
-        self.has_packet = False
+    def __init__(self, index, depth):
+        self.index = index
+        self.depth = depth
+        if not self.depth:
+            self.depth = None
 
-    def enter(self):
-        self.has_packet = True
-
-    def exit(self):
-        self.has_packet = False
-
-    def step(self):
-        if not self.state:
+    def found(self, timestamp):
+        if self.depth is None:
             return None
-        index = self.state.index(True)
-        self.state[index] = False
-        if (index == 0 and self.direction == -1) or (index == len(self.state) - 1 and self.direction == 1):
-            self.direction *= -1
-        new_index = index + self.direction
-        self.state[new_index] = True
-        if index == 0 and self.has_packet:
-            return len(self.state)
+        elif ((timestamp + self.index) % ((self.depth - 1) * 2)) == 0:
+            return self.index, self.depth
         else:
             return None
 
-    def reset(self):
-        if self.state:
-            self.state = [False for _ in range(len(self.state))]
-            self.state[0] = True
-            self.direction = 1
-            self.has_packet = False
-
     def __repr__(self):
-        return f"FirewallLayer({''.join(('T' if d else 'F' for d in self.state))}, {self.direction}, {'Packet' if self.has_packet else 'Empty'})"
+        return f"FirewallLayer(index={self.index}, depth={self.depth})"
 
 
 def build_firewall(recorded: str):
     ret_list = []
     for line in recorded.split('\n'):
         info = line.split(': ')
-        layer, size = int(info[0]), int(info[1])
-        if len(ret_list) < layer:
-            for _ in range(len(ret_list), layer):
-                ret_list.append(FirewallLayer(0))
-        ret_list.append(FirewallLayer(size))
+        ret_list.append(FirewallLayer(index=int(info[0]), depth=int(info[1])))
     return ret_list
 
 
-def run_through_firewall(firewall, delay=0):
-    if delay:
-        [[w.step() for w in firewall] for _ in range(delay)]
-    depth = 0
+def run_through_firewall(firewall, delay=0, fail_early=False):
     found = []
     for wall in firewall:
-        wall.enter()
-        # print(firewall)
-        wall_found = [(c, depth) for c in (w.step() for w in firewall) if c is not None]
+        wall_found = wall.found(delay)
         if wall_found:
-            found.append(wall_found[0])
-        wall.exit()
-        depth += 1
+            if fail_early:
+                return True
+            found.append(wall_found)
     if found:
         return sum(t[0] * t[1] for t in found)
     else:
@@ -119,20 +90,20 @@ def run_through_firewall(firewall, delay=0):
 
 
 def find_clean_run(firewall):
-    caught = False
+    caught = True
     i = 0
     while caught is not None:
-        [wall.reset() for wall in firewall]
-        caught = run_through_firewall(firewall, i)
-        if caught is None:
-            print(f"Not caught with delay {i}")
-        else:
-            print(f"caught score {caught} on run with delay {i}")
+        caught = run_through_firewall(firewall, i, fail_early=True)
+        # if caught is None:
+        #     print(f"Not caught with delay {i}")
+        # else:
+        #     print(f"caught score {caught} on run with delay {i}")
         i += 1
     return i - 1
 
 
-firewall = build_firewall(EXAMPLE_DATA)
-print(firewall)
-print(run_through_firewall(firewall))
-print(find_clean_run(firewall))
+if __name__ == '__main__':
+    firewall = build_firewall(DATA)
+    print(firewall)
+    print(run_through_firewall(firewall))
+    print(find_clean_run(firewall))
