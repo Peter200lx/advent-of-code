a78e746c977e0b5daf5362442b43581cf4a4def5
Peter Olson <peter.olson@gmail.com>
2018-12-12 14:01:13 -0800

    Day 12 switch to set for plants
    This stores significantly less data while still providing the same
    functionality. Also add printouts for visual analysis (but commented
    out for basic runs)

--- 2018/day12.py
+++ 2018/day12.py
@@ -53,16 +53,11 @@ EXAMPLE_DATA = """initial state: #..#.#..##......###...###
 
 
 PART_2_ITER = 50000000000
+FULL_RANGE = range(-3, 232)
 
 
 def parse_input(input_list):
-    row = {}
-    i = 0
-    for c in input_list[0]:
-        if c not in (".", "#"):
-            continue
-        row[i] = c
-        i += 1
+    row = {i for i, c in enumerate(input_list[0][15:]) if c == "#"}
     transforms = {}
     for input_str in input_list[2:]:
         before, after = input_str.split(" => ")
@@ -71,16 +66,14 @@ def parse_input(input_list):
 
 
 def run_transform(row, transforms):
-    bottom = min(row.keys())
-    top = max(row.keys())
-    new_dict = {}
+    bottom = min(row)
+    top = max(row)
+    new_set = set()
     for i in range(bottom - 3, top + 4):
-        check = "".join(row.get(j, ".") for j in range(i - 2, i + 3))
-        if check in transforms:
-            new_dict[i] = transforms[check]
-        elif bottom <= i <= top:
-            new_dict[i] = "."
-    return new_dict
+        check = "".join("#" if j in row else "." for j in range(i - 2, i + 3))
+        if transforms.get(check) == "#":
+            new_set.add(i)
+    return new_set
 
 
 def straight_line(x_vals, y_vals):
@@ -96,13 +89,15 @@ def straight_line(x_vals, y_vals):
 
 if __name__ == '__main__':
     plants, instructions = parse_input(DATA)
+    # print("".join(["#" if i in plants else " " for i in FULL_RANGE]))
     x_iters = []
     y_sums = []
     for iteration in range(1, 21):
         plants = run_transform(plants, instructions)
+        # print("".join(["#" if i in plants else " " for i in FULL_RANGE]))
         if iteration > 17:  # Prime the pump for part 2
             x_iters.append(iteration)
-            y_sums.append(sum([i for i in plants if plants[i] == "#"]))
+            y_sums.append(sum([i for i in plants]))
     print(f"Part 1: {y_sums[-1]}")
 
     for iteration in range(21, PART_2_ITER):
@@ -114,5 +109,6 @@ if __name__ == '__main__':
         x_iters.pop(0)
         y_sums.pop(0)
         plants = run_transform(plants, instructions)
+        # print("".join(["#" if i in plants else " " for i in FULL_RANGE]))
         x_iters.append(iteration)
-        y_sums.append(sum([i for i in plants if plants[i] == "#"]))
+        y_sums.append(sum([i for i in plants]))
