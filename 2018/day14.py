a2a708118945c623b08b6df9866b4ea602a93e4b
Peter Olson <peter.olson@gmail.com>
2018-12-13 22:44:13 -0800

    Day 14 improve readability

--- 2018/day14.py
+++ 2018/day14.py
@@ -1,4 +1,3 @@
-
 DATA = """637061"""
 SEED = [3, 7]
 
@@ -7,22 +6,26 @@ def combined_solution(seed_list, input_str, final=10):
     after = int(input_str)
     compare_list = list(int(c) for c in input_str)
     cur_list = seed_list[:]
-    elfs = [0, 1]
-    part_1 = False
-    part_2 = False
-    while not (part_1 and part_2):
+    elfs = list(range(len(seed_list)))
+    part_1_found = False
+    part_2_found = False
+    while not (part_1_found and part_2_found):
         elf_recipes = [cur_list[i] for i in elfs]
         new_recipes = [int(c) for c in str(sum(elf_recipes))]
         for recipe in new_recipes:
             cur_list.append(recipe)
-            if not part_2 and compare_list == cur_list[-len(compare_list):]:
+
+            if not part_2_found and compare_list == cur_list[-len(compare_list):]:
                 print(f"Part 2: {len(cur_list) - len(compare_list)}")
-                part_2 = True
-        for i, elf in enumerate(elfs):
-            elfs[i] = (elf + elf_recipes[i] + 1) % len(cur_list)
-        if not part_1 and len(cur_list) > after + final:
-            print(f"Part 1: {''.join(str(c) for c in cur_list[after: after + final])}")
-            part_1 = True
+                part_2_found = True
+
+        for i in range(len(elfs)):
+            elfs[i] += elf_recipes[i] + 1
+            elfs[i] %= len(cur_list)
+
+        if not part_1_found and len(cur_list) >= after + final:
+            print(f"Part 1: {''.join(str(c) for c in cur_list[after:after + final])}")
+            part_1_found = True
 
 
 if __name__ == '__main__':
