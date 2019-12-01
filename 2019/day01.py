b1919135a176579ae17aa9f323afcde1908ee079
Peter Olson <peter.olson@gmail.com>
2019-11-30 21:29:03 -0800

    Cleaned up day 1 logic

--- 2019/day01.py
+++ 2019/day01.py
@@ -3,30 +3,27 @@ from pathlib import Path
 
 def fuel_calc(num):
     fuel = (num // 3) - 2
-    if fuel < 0:
-        return 0
-    return fuel
+    return fuel if fuel > 0 else 0
 
 
-def calc(start, list_o_cal):
-    for i, num in enumerate(list_o_cal):
-        list_o_cal[i] = (num // 3) - 2
-    return sum(list_o_cal)
+def calc(list_o_mass):
+    for i, num in enumerate(list_o_mass):
+        list_o_mass[i] = fuel_calc(num)
+    return sum(list_o_mass)
 
 
-def calc_v2(start, list_o_cal):
-    for i, num in enumerate(list_o_cal):
-        list_o_cal[i] = (num // 3) - 2
-        fuel_add = fuel_calc(num)
+def calc_v2(list_o_mass):
+    for i, num in enumerate(list_o_mass):
+        list_o_mass[i] = fuel_add = fuel_calc(num)
         while fuel_add:
             fuel_add = fuel_calc(fuel_add)
-            list_o_cal[i] += fuel_add
-    return sum(list_o_cal)
+            list_o_mass[i] += fuel_add
+    return sum(list_o_mass)
 
 
 if __name__ == "__main__":
     DATA = Path("day01.input").read_text().strip()
-
     int_list = [int(i) for i in DATA.split("\n")]
-    print(calc(0, int_list[:]))
-    print(calc_v2(0, int_list[:]))
+
+    print(calc(int_list[:]))
+    print(calc_v2(int_list[:]))
