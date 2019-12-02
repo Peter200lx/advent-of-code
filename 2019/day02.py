1c3d9ce0f3053b7e61949215d9cfc00ed1893295
Peter Olson <peter.olson@gmail.com>
2019-12-01 21:21:56 -0800

    Cleanup protection logic for 2019 day 2

--- 2019/day02.py
+++ 2019/day02.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 
 def calc(list_o_codes, overrides):
+    list_o_codes = list_o_codes[:]
     for loc, val in overrides:
         list_o_codes[loc] = val
     index = 0
@@ -11,6 +12,8 @@ def calc(list_o_codes, overrides):
             list_o_codes[p] = list_o_codes[a] + list_o_codes[b]
         elif op == 2:
             list_o_codes[p] = list_o_codes[a] * list_o_codes[b]
+        else:
+            raise ValueError(f"Bad op code {op}")
         index += 4
     return list_o_codes[0]
 
@@ -18,7 +21,7 @@ def calc(list_o_codes, overrides):
 def find_state(list_o_codes, desired_state):
     for noun in range(100):
         for verb in range(100):
-            if calc(list_o_codes[:], ((1, noun), (2, verb))) == desired_state:
+            if calc(list_o_codes, ((1, noun), (2, verb))) == desired_state:
                 return noun, verb
 
 
@@ -26,6 +29,6 @@ if __name__ == "__main__":
     DATA = Path("day02.input").read_text().strip()
     int_list = [int(i) for i in DATA.split(",")]
 
-    print(calc(int_list[:], ((1, 12), (2, 2))))
+    print(calc(int_list, ((1, 12), (2, 2))))
     n, v = find_state(int_list, 19690720)
     print(n, v, 100 * n + v)
