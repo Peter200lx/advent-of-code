3064a3aa5d69bcd3f46b5004299b06b3f8274daf
Peter Olson <peter.olson@gmail.com>
2019-12-01 21:17:52 -0800

    Initial 2019 day 2 solution

---
+++ 2019/day02.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+
+def calc(list_o_codes, overrides):
+    for loc, val in overrides:
+        list_o_codes[loc] = val
+    index = 0
+    while list_o_codes[index] != 99:
+        op, a, b, p = list_o_codes[index : index + 4]
+        if op == 1:
+            list_o_codes[p] = list_o_codes[a] + list_o_codes[b]
+        elif op == 2:
+            list_o_codes[p] = list_o_codes[a] * list_o_codes[b]
+        index += 4
+    return list_o_codes[0]
+
+
+def find_state(list_o_codes, desired_state):
+    for noun in range(100):
+        for verb in range(100):
+            if calc(list_o_codes[:], ((1, noun), (2, verb))) == desired_state:
+                return noun, verb
+
+
+if __name__ == "__main__":
+    DATA = Path("day02.input").read_text().strip()
+    int_list = [int(i) for i in DATA.split(",")]
+
+    print(calc(int_list[:], ((1, 12), (2, 2))))
+    n, v = find_state(int_list, 19690720)
+    print(n, v, 100 * n + v)
