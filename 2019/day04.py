fb6a1c061aaef53602b04bdea6d88e6e5ffcd882
Peter Olson <peter.olson@gmail.com>
2019-12-03 21:23:22 -0800

    Cleaned up p1 vs p2 for 2019day4

--- 2019/day04.py
+++ 2019/day04.py
@@ -1,43 +1,21 @@
 from collections import Counter
 
 
-def validate(code):
+def validate(code, p2=False):
     if len(code) != 6:
         return False
     prev = 0
-    adjacent = False
     for char in code:
         cur = int(char)
-        if cur == prev:
-            adjacent = True
         if cur < prev:
             return False
         prev = cur
-    return adjacent
-
-
-def vp2(code):
-    if len(code) != 6:
-        return False
     c = Counter(code)
-    prev = 0
-    for char in code:
-        cur = int(char)
-        if cur < prev:
-            return False
-        prev = cur
-    return any(v == 2 for k, v in c.items())
+    return any(v > 1 if not p2 else v == 2 for v in c.values())
 
 
 if __name__ == "__main__":
     DATA = "372037-905157"
-    str_list = [int(num) for num in DATA.split("-")]
-    count = 0
-    cp2 = 0
-    for i in range(str_list[0], str_list[1] + 1):
-        if validate(str(i)):
-            count += 1
-        if vp2(str(i)):
-            cp2 += 1
-
-    print(count, cp2)
+    start, end = [int(num) for num in DATA.split("-")]
+    print(sum(validate(str(n)) for n in range(start, end + 1)))
+    print(sum(validate(str(n), p2=True) for n in range(start, end + 1)))
