208e008c6ed8bc35a4706956ab3882a498afdc37
Peter Olson <peter.olson@gmail.com>
2019-12-22 23:35:40 -0800

    2019day23 Code Cleanup & Typing

--- 2019/day23.py
+++ 2019/day23.py
@@ -1,10 +1,11 @@
 from pathlib import Path
+from typing import Iterator, List
 
 from processor import Processor, ProgramHalt
 
 
 class D23Processor(Processor):
-    def run_network_generator(self, nid, output_batch):
+    def run_network_generator(self, nid: int, output_batch: int) -> Iterator[List[int]]:
         ip = 0
         self.input.append(nid)
         try:
@@ -12,7 +13,7 @@ class D23Processor(Processor):
                 if self.memory[ip] % 10 == 3:  # If next instruction is input...
                     yield
                     if not self.input:
-                        self.input.append((-1))
+                        self.input.append(-1)
 
                 ip = self.func_by_instruction_pointer(ip)
 
@@ -23,45 +24,46 @@ class D23Processor(Processor):
             raise NotImplementedError(f"Don't expect the bot to ever halt the program")
 
 
-def run_network(program, num_comps: int, part_1=True) -> int:
+def run_network(program, num_comps: int = 50, part_2=False) -> int:
     machines = [D23Processor(program) for _ in range(num_comps)]
     processors = [bot.run_network_generator(n, 3) for n, bot in enumerate(machines)]
     [next(proc) for proc in processors]  # Move to first yield to accept .send()
-    nat_y_last = None
+    nat_y_last = nat_y = nat_x = None
     idle_check = 0
-    try:
-        while True:
-            any_output = False
-            for i in range(num_comps):
-                output = next(processors[i])
-                if output is not None:
-                    any_output = True
-                    address, X, Y = output
-                    if address == 255:
-                        if part_1:
-                            return Y
-                        else:
-                            natX, natY = X, Y
-                    else:
-                        machines[address].input += [X, Y]
+    while True:
+        any_output = False
+        for i in range(num_comps):
+            output = next(processors[i])
 
-            if not any_output and all(len(m.input) <= 1 for m in machines):
-                idle_check += 1
-                if idle_check > 2:
-                    idle_check = 0
-                    machines[0].input += [natX, natY]
-                    if natY == nat_y_last:
-                        return natY
+            if output is not None:
+                any_output = True
+                address, x, y = output
+                if 0 <= address < num_comps:
+                    machines[address].input += [x, y]
+                elif address == 255:
+                    if part_2:
+                        nat_x, nat_y = x, y
                     else:
-                        nat_y_last = natY
+                        return y
+                else:
+                    raise ValueError(f"Unknown output {output} from proc {i}")
 
-    except StopIteration:
-        raise NotImplementedError(f"Don't expect the bot to ever halt the program")
+        if not any_output and all(not m.input for m in machines):
+            idle_check += 1
+            if idle_check >= 2:
+                idle_check = 0
+                assert not (nat_x is nat_y is None)
+
+                machines[0].input += [nat_x, nat_y]
+                if nat_y == nat_y_last:
+                    return nat_y
+                else:
+                    nat_y_last = nat_y
 
 
 if __name__ == "__main__":
     DATA = Path("day23.input").read_text().strip()
     int_list = [int(i) for i in DATA.split(",")]
 
-    print(run_network(int_list, 50))
-    print(run_network(int_list, 50, part_1=False))
+    print(run_network(int_list))
+    print(run_network(int_list, part_2=True))
