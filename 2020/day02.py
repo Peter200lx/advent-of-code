3a950d48eeb05d9a9e13bde3c099037f940f0593
Peter Olson <peter.olson@gmail.com>
2020-12-01 21:15:58 -0800

    Initial 2020day02 solution

---
+++ 2020/day02.py
@@ -0,0 +1,32 @@
+from collections import Counter
+from pathlib import Path
+import re
+
+FILE_DIR = Path(__file__).parent
+
+LINE = re.compile(r"([0-9]+)-([0-9]+) ([a-z]+): (.*)")
+
+
+def parse_line(line):
+    match = LINE.match(line)
+    n1, n2, char, password = match.groups()
+    return int(n1), int(n2), char, password
+
+
+def validate_password(minnum, maxnum, char, password):
+    c = Counter(password)
+    if minnum <= c[char] <= maxnum:
+        return True
+    return False
+
+
+def real_validate_password(first, second, char, password):
+    a, b = (password[pos - 1] == char for pos in (first, second))
+    return a != b
+
+
+if __name__ == "__main__":
+    DATA = (FILE_DIR / "day02.input").read_text().strip()
+    INPUT = [parse_line(line) for line in DATA.split("\n")]
+    print(sum(validate_password(*t) for t in INPUT))
+    print(sum(real_validate_password(*t) for t in INPUT))
