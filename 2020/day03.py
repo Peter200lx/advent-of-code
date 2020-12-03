8f91fb799d6242a2ab70a591bde1dba9b7e65c63
Peter Olson <peter.olson@gmail.com>
2020-12-02 21:31:23 -0800

    Use Coord to simplify addition 2020day03

--- 2020/day03.py
+++ 2020/day03.py
@@ -1,7 +1,16 @@
 import math
 from pathlib import Path
+from typing import NamedTuple
 
 FILE_DIR = Path(__file__).parent
+
+
+class Coord(NamedTuple):
+    x: int
+    y: int
+
+    def __add__(self, other: "Coord") -> "Coord":
+        return Coord(self.x + other.x, self.y + other.y)
 
 
 def read_map(lines):
@@ -10,25 +19,24 @@ def read_map(lines):
     x_max = len(lines[0])
     for y, line in enumerate(lines):
         for x, c in enumerate(line):
-            x_max = max(x_max, x)
             if c == "#":
-                tree_pos.add((x, y))
+                tree_pos.add(Coord(x, y))
     return x_max, y_max, tree_pos
 
 
-def move_check(width, end_y, tree_pos, slope=(3, 1)):
-    loc = (0, 0)
+def move_check(width, end_y, tree_pos, slope=Coord(3, 1)):
+    loc = Coord(0, 0)
     count = 0
-    while loc[1] <= end_y:
-        tree_x = loc[0] % width
-        if (tree_x, loc[1]) in tree_pos:
+    while loc.y <= end_y:
+        tree_loc = Coord(loc.x % width, loc.y)
+        if tree_loc in tree_pos:
             count += 1
-        loc = loc[0] + slope[0], loc[1] + slope[1]
+        loc += slope
     return count
 
 
 def check_all(width, end_y, tree_pos):
-    counts = [move_check(width, end_y, tree_pos, slope) for slope in ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))]
+    counts = [move_check(width, end_y, tree_pos, Coord(*slope)) for slope in ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))]
     return math.prod(counts)
 
 
