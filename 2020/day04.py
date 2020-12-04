5a5e9803933d46fe463563aab781b193708f3040
Peter Olson <peter.olson@gmail.com>
2020-12-04 00:24:21 -0800

    Use validator dict 2020day04

--- 2020/day04.py
+++ 2020/day04.py
@@ -3,11 +3,19 @@ from pathlib import Path
 
 FILE_DIR = Path(__file__).parent
 
-
-FIELDS = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"}
-REQUIRED_FIELDS = FIELDS - {"cid"}
-
 EYE_COLORS = {"amb", "blu", "brn", "gry", "grn", "hzl", "hcl", "oth"}
+
+FIELDS = {
+    "byr": lambda v: 1920 <= int(v) <= 2002,
+    "iyr": lambda v: 2010 <= int(v) <= 2020,
+    "eyr": lambda v: 2020 <= int(v) <= 2030,
+    "hgt": lambda v: v.endswith("cm") and 150 <= int(v[:-2]) <= 193 or v.endswith("in") and 59 <= int(v[:-2]) <= 76,
+    "hcl": lambda v: re.match(r"#[0-9a-f]{6}", v),
+    "ecl": lambda v: v in EYE_COLORS,
+    "pid": lambda v: len(v) == 9 and v.isnumeric(),
+    "cid": lambda v: True,
+}
+REQUIRED_FIELDS = FIELDS.keys() - {"cid"}
 
 
 def read_passports(lines):
@@ -36,28 +44,9 @@ def valid_passport(passport):
     if not REQUIRED_FIELDS <= passport.keys():
         return False
     try:
-        if not 1920 <= int(passport["byr"]) <= 2002:
-            return False
-        if not 2010 <= int(passport["iyr"]) <= 2020:
-            return False
-        if not 2020 <= int(passport["eyr"]) <= 2030:
-            return False
-        hgt = passport["hgt"]
-        if hgt.endswith("cm"):
-            if not 150 <= int(hgt[:-2]) <= 193:
+        for k, v in passport.items():
+            if not FIELDS[k](v):
                 return False
-        elif hgt.endswith("in"):
-            if not 59 <= int(hgt[:-2]) <= 76:
-                return False
-        else:
-            return False
-        if not re.match(r"#[0-9a-f]{6}", passport["hcl"]):
-            return False
-        if passport["ecl"] not in EYE_COLORS:
-            return False
-        if len(passport["pid"]) != 9:
-            return False
-        int(passport["pid"])
     except ValueError:
         return False
     return True
