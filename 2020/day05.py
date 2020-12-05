e9b2834c04131a461cb2e76eaae1f6040dee41d1
Peter Olson <peter.olson@gmail.com>
2020-12-04 22:40:08 -0800

    Use translate instead of multiple replace 2020day05

--- 2020/day05.py
+++ 2020/day05.py
@@ -2,13 +2,12 @@ from pathlib import Path
 
 FILE_DIR = Path(__file__).parent
 
+TRANSLATOR = "".maketrans("FLBR", "0011")
+
 
 if __name__ == "__main__":
     DATA = (FILE_DIR / "day05.input").read_text().strip()
-    SEATS = {
-        int(line.replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1"), 2)
-        for line in DATA.split("\n")
-    }
+    SEATS = {int(line.translate(TRANSLATOR), 2) for line in DATA.split("\n")}
     max_seat = max(SEATS)
     print(max_seat)
     print([x for x in range(min(SEATS), max_seat) if x not in SEATS])
