7083ed5890b2b89f81ce6ba2a3fa17ba653c1f88
Peter Olson <peter.olson@gmail.com>
2020-12-05 21:20:13 -0800

    Cleaned up 2020day06 solution

--- 2020/day06.py
+++ 2020/day06.py
@@ -3,15 +3,8 @@ from pathlib import Path
 FILE_DIR = Path(__file__).parent
 
 
-def combine_groups(list_set):
-    s = list_set[0]
-    for item in list_set:
-        s &= item
-    return s
-
-
 if __name__ == "__main__":
     DATA = (FILE_DIR / "day06.input").read_text().strip()
     ANSWERS = [[{c for c in person} for person in group.split("\n")] for group in DATA.split("\n\n")]
-    print(sum(len({c for p in g for c in p}) for g in ANSWERS))
-    print(sum(len(combine_groups(g)) for g in ANSWERS))
+    print(sum(len(set.union(*g)) for g in ANSWERS))
+    print(sum(len(set.intersection(*g)) for g in ANSWERS))
