e031f93d420170b28c2b5e1b21a47a9386d1f552
Peter Olson <peter.olson@gmail.com>
2020-12-08 20:47:25 -0800

    Switch to NamedTuple for instructions 2020day08

--- 2020/day08.py
+++ 2020/day08.py
@@ -1,11 +1,24 @@
 from pathlib import Path
-from typing import List, Tuple
+from typing import List, NamedTuple
 
 FILE_DIR = Path(__file__).parent
 
 
+class Instruction(NamedTuple):
+    op: str
+    value: int
+
+    def invert(self):
+        if self.op == "jmp":
+            return Instruction("nop", self.value)
+        elif self.op == "nop":
+            return Instruction("jmp", self.value)
+        else:
+            return self
+
+
 class Processor:
-    def __init__(self, program: List[Tuple[str, int]]):
+    def __init__(self, program: List[Instruction]):
         self.program = program
         self.acc = 0
         self.mapping = {m[3:]: getattr(self, m) for m in dir(self) if m.startswith("op_")}
@@ -24,26 +37,22 @@ class Processor:
         visited_ip = set()
         while ip not in visited_ip:
             visited_ip.add(ip)
-            op, v = self.program[ip]
-            ip = self.func_by_instruction_pointer(ip, op, v)
+            ip = self.func_by_instruction_pointer(ip, self.program[ip])
             if ip >= len(self.program):
                 return True
         return False
 
-    def func_by_instruction_pointer(self, ip, op: str, value: int) -> int:
-        new_ip = self.mapping[op](value)
+    def func_by_instruction_pointer(self, ip, inst: Instruction) -> int:
+        new_ip = self.mapping[inst.op](inst.value)
         return ip + (1 if new_ip is None else new_ip)
 
 
-def part_2(instructions: List[Tuple[str, int]]):
+def part_2(instructions: List[Instruction]):
     for i in range(len(instructions)):
-        if instructions[i][0] == "acc":
+        if instructions[i].op == "acc":
             continue
         my_inst = instructions.copy()
-        if instructions[i][0] == "jmp":
-            my_inst[i] = ("nop", instructions[i][1])
-        elif instructions[i][0] == "nop":
-            my_inst[i] = ("jmp", instructions[i][1])
+        my_inst[i] = instructions[i].invert()
         proc = Processor(my_inst)
         if proc.run():
             print(i, instructions[i])
@@ -53,7 +62,7 @@ def part_2(instructions: List[Tuple[str, int]]):
 
 if __name__ == "__main__":
     DATA = (FILE_DIR / "day08.input").read_text().strip()
-    INST = [(op, int(val)) for inst in DATA.split("\n") for op, val in [inst.split()]]
+    INST = [Instruction(op, int(val)) for inst in DATA.split("\n") for op, val in [inst.split()]]
     part_1 = Processor(INST)
     part_1.run()
     print(part_1.acc)
