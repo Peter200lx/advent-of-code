5c552c3efbd84e867936ebf27a41e86e18c24b80
Peter Olson <peter.olson@gmail.com>
2020-12-11 23:51:59 -0800

    Type and Simplify 2020day12
    Also use proper exception in __mul__ method

--- 2020/day12.py
+++ 2020/day12.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import NamedTuple
+from typing import NamedTuple, Union
 
 FILE_DIR = Path(__file__).parent
 
@@ -16,17 +16,17 @@ class Coord(NamedTuple):
     def __add__(self, other: "Coord") -> "Coord":
         return Coord(self.x + other.x, self.y + other.y)
 
-    def __mul__(self, other) -> "Coord":
+    def __mul__(self, other: Union[int, "Coord"]) -> "Coord":
         if isinstance(other, int):
             return Coord(self.x * other, self.y * other)
         elif isinstance(other, Coord):
             return Coord(self.x * other.x, self.y * other.y)
-        raise NotImplementedError
+        raise NotImplemented
 
-    def flip(self):
+    def flip(self) -> "Coord":
         return Coord(x=self.y, y=self.x)
 
-    def manhattan_distance(self, other) -> int:
+    def manhattan_distance(self, other: "Coord") -> int:
         return abs(self.x + other.x) + abs(self.y + other.y)
 
 
@@ -48,15 +48,17 @@ ROTATION_WAYPOINT = {
 }
 
 
-WAYPOINT_START_DELTA = Coord(x=10, y=1)
+START_DIRECTION = "E"
+START_LOCATION = Coord(x=0, y=0)
+START_WAYPOINT_DELTA = Coord(x=10, y=1)
 
 
 class Ship:
     def __init__(self):
-        self.facing = "E"
-        self.location = Coord(0, 0)
-        self.locations_been = [self.location]
-        self.waypoint_delta = WAYPOINT_START_DELTA
+        self.facing = START_DIRECTION
+        self.start_location = START_LOCATION
+        self.location = self.start_location
+        self.waypoint_delta = START_WAYPOINT_DELTA
 
     def move_p1(self, instruction: Instruction):
         if instruction.type in DIR_MAP:
@@ -68,23 +70,21 @@ class Ship:
             rot_seq = ROTATION_MAP[instruction.type]
             i = rot_seq.index(self.facing)
             self.facing = rot_seq[(i + rot_index) % 4]
-        self.locations_been.append(self.location)
 
     def read_instruction(self, instruction: Instruction):
         if instruction.type in DIR_MAP:
             self.waypoint_delta += DIR_MAP[instruction.type] * instruction.num
         elif instruction.type in ROTATION_WAYPOINT:
             rot_index = (instruction.num // 90) % 4
-            if rot_index in (1, 3):
+            if rot_index in (1, 3):  # 90 and 180 degrees or multiples thereof
                 self.waypoint_delta = self.waypoint_delta.flip()
             rot_seq = ROTATION_WAYPOINT[instruction.type]
             self.waypoint_delta *= rot_seq[rot_index]
         elif instruction.type == "F":
             self.location += self.waypoint_delta * instruction.num
-            self.locations_been.append(self.location)
 
-    def man_from_start(self):
-        return self.location.manhattan_distance(self.locations_been[0])
+    def man_from_start(self) -> int:
+        return self.location.manhattan_distance(self.start_location)
 
 
 EXAMPLE_DATA = """F10
