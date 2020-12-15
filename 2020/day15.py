5f139980a6182eeeddc0f77ec62ff62da98daaba
Peter Olson <peter.olson@gmail.com>
2020-12-14 23:57:05 -0800

    User enumerate `start=` parameter 2020day15

--- 2020/day15.py
+++ 2020/day15.py
@@ -4,7 +4,7 @@ DATA = """16,12,1,0,15,7,11"""
 
 
 def solve(numbers: List[int], find: int) -> int:
-    last_spoken_time = {n: i + 1 for i, n in enumerate(numbers[:-1])}
+    last_spoken_time = {n: i for i, n in enumerate(numbers[:-1], start=1)}
     last_num = numbers[-1]
     for i in range(len(numbers), find):
         next_num = i - last_spoken_time.get(last_num, i)
