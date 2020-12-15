d329275e974f1177f1076ff0f955c93ac4359a56
Peter Olson <peter.olson@gmail.com>
2020-12-14 23:40:33 -0800

    Simplify & Type 2020day15

--- 2020/day15.py
+++ 2020/day15.py
@@ -1,19 +1,20 @@
-def solve(numbers, find):
+from typing import List
+
+DATA = """16,12,1,0,15,7,11"""
+
+
+def solve(numbers: List[int], find: int) -> int:
     last_spoken_time = {n: i + 1 for i, n in enumerate(numbers[:-1])}
     last_num = numbers[-1]
     for i in range(len(numbers), find):
-        if last_num not in last_spoken_time:
-            next_num = 0
-        else:
-            next_num = i - last_spoken_time[last_num]
+        next_num = i - last_spoken_time.get(last_num, i)
         last_spoken_time[last_num] = i
         last_num = next_num
     return last_num
 
 
 if __name__ == "__main__":
-    DATA = """16,12,1,0,15,7,11"""
-    NUMBER = [int(i) for i in DATA.split(",")]
+    NUMBERS = [int(i) for i in DATA.split(",")]
 
-    print(solve(NUMBER, 2020))
-    print(solve(NUMBER, 30000000))
+    print(solve(NUMBERS, 2020))
+    print(solve(NUMBERS, 30000000))
