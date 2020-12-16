266a89750e973e69918a1d8f7f31261b093d08d6
Peter Olson <peter.olson@gmail.com>
2020-12-15 22:21:56 -0800

    Simplify 2020day16

--- 2020/day16.py
+++ 2020/day16.py
@@ -1,7 +1,7 @@
 import re
 from collections import defaultdict
 from pathlib import Path
-from typing import NamedTuple, Tuple, List
+from typing import NamedTuple, Tuple, List, Dict
 
 FILE_DIR = Path(__file__).parent
 
@@ -25,34 +25,34 @@ class Field(NamedTuple):
         return any(r.valid(num) for r in self.ranges)
 
 
-def parse_input(lines):
-    ranges, your, near = lines.split("\n\n")
-    ranges_dict = {}
-    for line in ranges.split("\n"):
+def parse_input(lines) -> Tuple[List[Field], List[int], List[List[int]]]:
+    fields, your, near = lines.split("\n\n")
+    populated_fields = []
+    for line in fields.split("\n"):
         if not line:
             continue
         name, nums = line.split(": ")
         range_list = []
-        for rang in nums.split(" or "):
-            r1, r2 = rang.split("-")
-            range_list.append(Range(int(r1), int(r2)))
+        for section in nums.split(" or "):
+            n1, n2 = section.split("-")
+            range_list.append(Range(int(n1), int(n2)))
         assert len(range_list) == 2, "Always only two ranges"
-        ranges_dict[name] = Field(name, tuple(range_list))
-    your_tickets = []
+        populated_fields.append(Field(name, tuple(range_list)))
+    your_ticket = []
     for line in your.split("\n"):
         if "," not in line:
             continue
-        your_tickets = list(map(int, RE_NUMS.findall(line)))
+        your_ticket = list(map(int, RE_NUMS.findall(line)))
     near_tickets = []
     for line in near.split("\n"):
         if "," not in line:
             continue
         near_tickets.append(list(map(int, RE_NUMS.findall(line))))
-    return ranges_dict, your_tickets, near_tickets
+    return populated_fields, your_ticket, near_tickets
 
 
-def calculate_order(all_fields: List[Field], valid_nearby_lines: List[List[int]]):
-    possible_positions = [list(all_fields) for _ in range(len(valid_nearby_lines[0]))]
+def calculate_order(all_fields: List[Field], valid_nearby_lines: List[List[int]]) -> Dict[Field, int]:
+    possible_positions = [all_fields.copy() for _ in range(len(valid_nearby_lines[0]))]
     for ticket_line in valid_nearby_lines:
         for i, number in enumerate(ticket_line):
             still_possible = [f for f in possible_positions[i] if f.valid(number)]
@@ -73,26 +73,30 @@ def calculate_order(all_fields: List[Field], valid_nearby_lines: List[List[int]]
                 if to_remove_i in possible_dict[key]:
                     possible_dict[key].discard(to_remove_i)
 
-    return possible_dict
+    return {field: next(iter(locs)) for field, locs in possible_dict.items()}
+
+
+def run_logic(input_str: str):
+    fields, your_ticket, near_tickets = parse_input(input_str)
+    part_1 = 0
+    valid_tickets = []
+    for ticket_line in near_tickets:
+        valid_line = True
+        for ticket in ticket_line:
+            if any(field.valid(ticket) for field in fields):
+                continue
+            part_1 += ticket
+            valid_line = False
+        if valid_line:
+            valid_tickets.append(ticket_line)
+    print(part_1)
+    positions = calculate_order(list(fields), valid_tickets)
+    part_2 = 1
+    for pos in (i for p, i in positions.items() if p.name.startswith("departure ")):
+        part_2 *= your_ticket[pos]
+    print(part_2)
 
 
 if __name__ == "__main__":
     DATA = (FILE_DIR / "day16.input").read_text().strip()
-    r, y, n = parse_input(DATA)
-    total = 0
-    valid = []
-    for ticket_line in n:
-        valid_line = True
-        for ticket in ticket_line:
-            if any(rang.valid(ticket) for rang in r.values()):
-                continue
-            total += ticket
-            valid_line = False
-        if valid_line:
-            valid.append(ticket_line)
-    print(total)
-    positions = calculate_order(list(r.values()), valid)
-    part_2 = 1
-    for pos in (next(iter(i)) for p, i in positions.items() if p.name.startswith("departure ")):
-        part_2 *= y[pos]
-    print(part_2)
+    run_logic(DATA)
