5148a61663d066df84f60b97b49c2c06ec2fe204
Peter Olson <peter.olson@gmail.com>
2020-12-27 20:55:53 -0800

    Simplify 2015day20 solution

--- 2015/day20.py
+++ 2015/day20.py
@@ -1,22 +1,21 @@
 DATA = """34000000"""
 
 
-def find_min(above: int, part_2=False):
+def find_min(above: int):
     locs = [0] * (above // 10)
+    locs_p2 = [0] * (above // 10)
     for i in range(1, above // 10):
         visited = 0
         for j in range(i, above // 10, i):
-            if part_2:
-                locs[j] += 11 * i
+            if visited <= 50:
+                locs_p2[j] += 11 * i
                 visited += 1
-                if visited > 50:
-                    break
-            else:
-                locs[j] += 10 * i
-    return next(i for i, v in enumerate(locs) if v > above)
+            locs[j] += 10 * i
+    return tuple(next(i for i, v in enumerate(L) if v > above) for L in (locs, locs_p2))
 
 
 if __name__ == "__main__":
     MIN_PRES = int(DATA)
-    print(find_min(MIN_PRES))
-    print(find_min(MIN_PRES, part_2=True))
+    part_1, part_2 = find_min(MIN_PRES)
+    print(part_1)
+    print(part_2)
