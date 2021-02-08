35f0eeb632bc3a4985817663d23465d95201ecbb
Peter Olson <peter.olson@gmail.com>
2021-02-07 18:51:57 -0800

    Improved 2016day21 solution

--- 2016/day21.py
+++ 2016/day21.py
@@ -8,23 +8,37 @@ SCRAMBLE_START = "abcdefgh"
 ALREADY_SCRAMBLED = "fbgdceah"
 
 
+def _handle_swap(cur_str: List[str], words: List[str]):
+    if words[1] == "position":
+        ind_x, ind_y = int(words[2]), int(words[5])
+    elif words[1] == "letter":
+        x, y = words[2], words[5]
+        ind_x, ind_y = cur_str.index(x), cur_str.index(y)
+    else:
+        raise NotImplementedError
+    cur_str[ind_x], cur_str[ind_y] = cur_str[ind_y], cur_str[ind_x]
+
+
+def _handle_reverse(cur_str: List[str], words: List[str]):
+    x, y = int(words[2]), int(words[4])
+    rev_stop = x - 1 if x - 1 >= 0 else None
+    return cur_str[:x] + cur_str[y:rev_stop:-1] + cur_str[y + 1 :]
+
+
+def _handle_move(cur_str: List[str], from_index: int, to_index: int):
+    letter = cur_str[from_index]
+    cur_str.remove(letter)
+    cur_str.insert(to_index, letter)
+
+
 def interpret_moves(instructions: str, start: str = SCRAMBLE_START) -> str:
     cur_str: List[str] = list(start)
     reverse_map: Dict[int, List[List[Union[str, int]]]] = {n: [] for n in range(len(start))}
     for line in instructions.split("\n"):
         reverse_debug = None
         words = line.split()
-        if not words:
-            continue
         if words[0] == "swap":
-            if words[1] == "position":
-                ind_x, ind_y = int(words[2]), int(words[5])
-            elif words[1] == "letter":
-                x, y = words[2], words[5]
-                ind_x, ind_y = cur_str.index(x), cur_str.index(y)
-            else:
-                raise NotImplementedError
-            cur_str[ind_x], cur_str[ind_y] = cur_str[ind_y], cur_str[ind_x]
+            _handle_swap(cur_str, words)
         elif words[0] == "rotate":
             if words[1] == "left":
                 steps = int(words[2])
@@ -45,14 +59,9 @@ def interpret_moves(instructions: str, start: str = SCRAMBLE_START) -> str:
             if reverse_debug:
                 reverse_debug.extend(["".join(cur_str), cur_str.index(based_on), cur_str.index(based_on) - ind_base])
         elif words[0] == "reverse":
-            x, y = int(words[2]), int(words[4])
-            rev_stop = x - 1 if x - 1 >= 0 else None
-            cur_str = cur_str[:x] + cur_str[y:rev_stop:-1] + cur_str[y + 1 :]
+            cur_str = _handle_reverse(cur_str, words)
         elif words[0] == "move":
-            x, y = int(words[2]), int(words[5])
-            letter = cur_str[x]
-            cur_str.remove(letter)
-            cur_str.insert(y, letter)
+            _handle_move(cur_str, int(words[2]), int(words[5]))
         else:
             raise NotImplementedError
     # pprint(reverse_map)
@@ -66,39 +75,23 @@ def reverse_scramble(instructions: str, end: str = ALREADY_SCRAMBLED) -> str:
     cur_str: List[str] = list(end)
     for line in reversed(instructions.split("\n")):
         words = line.split()
-        if not words:
-            continue
         if words[0] == "swap":  # ok
-            if words[1] == "position":
-                ind_x, ind_y = int(words[2]), int(words[5])
-            elif words[1] == "letter":
-                x, y = words[2], words[5]
-                ind_x, ind_y = cur_str.index(x), cur_str.index(y)
-            else:
-                raise NotImplementedError
-            cur_str[ind_x], cur_str[ind_y] = cur_str[ind_y], cur_str[ind_x]
+            _handle_swap(cur_str, words)
         elif words[0] == "rotate":
             if words[1] == "left":  # ok
                 steps = -int(words[2])
             elif words[1] == "right":  # ok
                 steps = int(words[2])
             elif words[1] == "based":
-                based_on = words[6]
-                ind_base = cur_str.index(based_on)
-                steps = REVERSE_MAP[ind_base]
+                steps = REVERSE_MAP[cur_str.index(words[6])]
             else:
                 raise NotImplementedError
             steps %= len(cur_str)
             cur_str = cur_str[steps:] + cur_str[:steps]
         elif words[0] == "reverse":  # ok
-            x, y = int(words[2]), int(words[4])
-            rev_stop = x - 1 if x - 1 >= 0 else None
-            cur_str = cur_str[:x] + cur_str[y:rev_stop:-1] + cur_str[y + 1 :]
+            cur_str = _handle_reverse(cur_str, words)
         elif words[0] == "move":  # ok
-            x, y = int(words[2]), int(words[5])
-            letter = cur_str[y]
-            cur_str.remove(letter)
-            cur_str.insert(x, letter)
+            _handle_move(cur_str, int(words[5]), int(words[2]))
         else:
             raise NotImplementedError
     return "".join(cur_str)
