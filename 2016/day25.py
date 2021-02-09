5fbb0a3a5eb579ac31465837969f1a13c8a03128
Peter Olson <peter.olson@gmail.com>
2021-02-08 20:03:00 -0800

    Initial 2016day25 solution

---
+++ 2016/day25.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+from typing import List, Union
+
+import day12
+
+FILE_DIR = Path(__file__).parent
+
+
+def fast_simulate(a: int, b: int, c: int) -> bool:
+    num_produced = 0
+    last_produced = 1
+    d = a + c * b
+    while True:
+        a = d
+        while a != 0:
+            b = a
+            a = b // 2
+            c = b % 2
+            if c == last_produced:
+                return False
+            last_produced = c
+            num_produced += 1
+            if num_produced > 100:
+                return True
+
+
+def find_signal(program: List[List[Union[str, int]]]):
+    c = program[1][1]
+    b = program[2][1]
+    for i in range(999999):
+        if fast_simulate(i, b, c):
+            return i
+
+
+if __name__ == '__main__':
+    DATA = (FILE_DIR / "day25.input").read_text().strip()
+    print(find_signal(day12.parse_data(DATA)))
