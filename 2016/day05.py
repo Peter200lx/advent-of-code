69576e6ce06579cbcd2101ffa8b2eea8d6199baf
Peter Olson <peter.olson@gmail.com>
2021-02-22 00:00:09 -0800

    Cleanup 2016day05 with typing and pre-computation

--- 2016/day05.py
+++ 2016/day05.py
@@ -1,11 +1,15 @@
 from hashlib import md5
+from typing import Optional, Tuple
 
-example_data = "abc"
-data = "uqwqemis"
+EXAMPLE_DATA = "abc"
+DATA = "uqwqemis"
+
+CODE_LENGTH = 8
+VALID_LOC = {str(i) for i in range(CODE_LENGTH)}
 
 
-def calc_digit(door_id, num):
-    mystr = str(door_id) + str(num)
+def calc_digit(door_id: str, num: int) -> Optional[str]:
+    mystr = f"{door_id}{num}"
     myhash = md5(mystr.encode("utf-8")).hexdigest()
     if myhash.startswith("00000"):
         print(f"{mystr} --hash-> {myhash}")
@@ -13,34 +17,38 @@ def calc_digit(door_id, num):
     return None
 
 
-def calc_digit2(door_id, num):
-    mystr = str(door_id) + str(num)
+def calc_digit2(door_id: str, num: int) -> Tuple[Optional[int], Optional[str]]:
+    mystr = f"{door_id}{num}"
     myhash = md5(mystr.encode("utf-8")).hexdigest()
-    if myhash.startswith("00000") and myhash[5] in (str(i) for i in range(8)):
+    if myhash[5] in VALID_LOC and myhash.startswith("00000"):
         print(f"{mystr} --hash-> {myhash}")
         return int(myhash[5]), myhash[6]
-    return None
+    return None, None
 
 
 if __name__ == "__main__":
     code = ""
     for i in range(29999999):
-        code += calc_digit(data, i) or ""
-        if i % 1000000 == 0:
+        if i % 1_000_000 == 0:
             print(f"{i} currently >{code}<")
-        if len(code) >= 8:
+        code_val = calc_digit(DATA, i)
+        if code_val is None:
+            continue
+        code += code_val
+        if len(code) >= CODE_LENGTH:
             break
 
     print(f"--- Answer is: {code}\n\n")
 
-    complex_code = [None for i in range(8)]
+    complex_code = [None] * CODE_LENGTH
     for i in range(29999999):
-        loc_code = calc_digit2(data, i)
-        if loc_code is not None and complex_code[loc_code[0]] is None:
-            complex_code[loc_code[0]] = loc_code[1]
-            if None not in complex_code:
-                break
-        if i % 1000000 == 0:
+        if i % 1_000_000 == 0:
             print(f"{i} currently >{complex_code}<")
+        code_loc, code_val = calc_digit2(DATA, i)
+        if code_loc is None or complex_code[code_loc] is not None:
+            continue
+        complex_code[code_loc] = code_val
+        if None not in complex_code:
+            break
 
     print(f"--- Answer is: {''.join(complex_code)}")
