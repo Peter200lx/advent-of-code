40d158aeb359c8472577f286ac603e71201af02b
Peter Olson <peter.olson@gmail.com>
2021-11-30 21:23:13 -0800

    Initial 2021day01 solution

---
+++ 2021/day01.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from typing import List
+
+FILE_DIR = Path(__file__).parent
+
+
+def count_inc(input_list: List[int]) -> int:
+    prev = input_list[0]
+    count = 0
+    for n in input_list:
+        if n > prev:
+            count += 1
+        prev = n
+    return count
+
+
+def sliding_window(input_list: List[int], window_size: int = 3) -> List[int]:
+    sliding_counts = []
+    for i, _n in enumerate(input_list):
+        try:
+            sliding_counts.append(sum(input_list[i : i + window_size]))
+        except KeyError:
+            break
+    return sliding_counts
+
+
+if __name__ == "__main__":
+    DATA = (FILE_DIR / "day01.input").read_text().strip()
+    INPUT_INTS = [int(i) for i in DATA.split("\n")]
+
+    print(count_inc(INPUT_INTS))
+
+    print(count_inc(sliding_window(INPUT_INTS)))
