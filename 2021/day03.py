461bd06f45c0f15cd88f00eac6df82b9a9cbae18
Peter Olson <peter.olson@gmail.com>
2021-12-02 21:36:12 -0800

    Initial 2021day03 solution

---
+++ 2021/day03.py
@@ -0,0 +1,47 @@
+from collections import Counter
+from pathlib import Path
+
+FILE_DIR = Path(__file__).parent
+
+
+def part1(lines):
+    counters = [Counter() for _ in range(len(lines[0]))]
+    for line in lines:
+        for i, c in enumerate(line):
+            counters[i][c] += 1
+
+    most = "".join(count.most_common()[0][0] for count in counters)
+    least = "".join(count.most_common()[-1][0] for count in counters)
+    return most, least
+
+
+def part2(lines):
+    oxy_pool = co2_pool = lines
+    for i in range(len(lines[0])):
+        if len(oxy_pool) > 1:
+            oxy_mc = Counter(l[i] for l in oxy_pool).most_common()
+            if oxy_mc[0][1] == oxy_mc[1][1]:
+                oxy_digit = "1"
+            else:
+                oxy_digit = oxy_mc[0][0]
+            oxy_pool = [l for l in oxy_pool if l[i] == oxy_digit]
+        if len(co2_pool) > 1:
+            co2_mc = Counter(l[i] for l in co2_pool).most_common()
+            if co2_mc[0][1] == co2_mc[1][1]:
+                co2_digit = "0"
+            else:
+                co2_digit = co2_mc[-1][0]
+            co2_pool = [l for l in co2_pool if l[i] == co2_digit]
+    return oxy_pool[0], co2_pool[0]
+
+
+if __name__ == "__main__":
+    DATA = (FILE_DIR / "day03.input").read_text().strip()
+    INPUT = [i for i in DATA.split("\n")]
+
+    MOST_COMMON, LEAST_COMMON = part1(INPUT)
+
+    print(int(MOST_COMMON, base=2) * int(LEAST_COMMON, base=2))
+
+    OXY_RATING, CO2_RATING = part2(INPUT)
+    print(int(OXY_RATING, base=2) * int(CO2_RATING, base=2))
