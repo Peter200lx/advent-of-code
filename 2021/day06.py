bb530403a09b0b00655740a63cf507bc39d2dc08
Peter Olson <peter.olson@gmail.com>
2021-12-05 21:54:56 -0800

    Use neat modulo trick from ruby solution 2021day06
    https://www.reddit.com/r/adventofcode/comments/r9z49j/2021_day_6_solutions/hnfdhsi/

--- 2021/day06.py
+++ 2021/day06.py
@@ -6,10 +6,10 @@ FILE_DIR = Path(__file__).parent
 
 def solve(fish_ages: List[int], days: int) -> int:
     count_of_fish_in_age = [fish_ages.count(i) for i in range(9)]
+    # I really like the modulo approach from this ruby solution
+    # https://www.reddit.com/r/adventofcode/comments/r9z49j/2021_day_6_solutions/hnfdhsi/
     for i in range(days):
-        day_count = count_of_fish_in_age.pop(0)
-        count_of_fish_in_age[6] += day_count
-        count_of_fish_in_age.append(day_count)
+        count_of_fish_in_age[(i + 7) % 9] += count_of_fish_in_age[i % 9]
     return sum(count_of_fish_in_age)
 
 
