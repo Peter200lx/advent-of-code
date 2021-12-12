ec16d2d29af391e06ccddf840522339bb52a37af
Peter Olson <peter.olson@gmail.com>
2021-12-11 23:02:52 -0800

    Clean up part 2 small cave dupe logic 2021day12

--- 2021/day12.py
+++ 2021/day12.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from pathlib import Path
-from typing import List, Tuple, Set
+from typing import List, Set
 
 INPUT_FILE = Path(__file__).with_suffix(".input")
 
@@ -14,20 +14,15 @@ class Cave:
     def __hash__(self):
         return self.name.__hash__()
 
-    def __init__(self, name: str):
-        self.name = name
-        self.adjacent = set()
-        if name[0].isupper():
-            self.large = True
-
     def navigate_to_end(
-        self, my_path: List[str] = None, list_o_paths: List[List[str]] = None, dupe_cave: str = None
+        self, my_path: List[str] = None, list_o_paths: List[List[str]] = None, dupe_cave: bool = None
     ) -> List[List[str]]:
         my_path = my_path or []
         list_o_paths = list_o_paths if list_o_paths is not None else []
         if not self.large and self.name in my_path:
-            if not (self.name == dupe_cave and my_path.count(self.name) == 1):
+            if dupe_cave is None or dupe_cave or self.name in {"start", "end"}:
                 return list_o_paths
+            dupe_cave = True
         my_path.append(self.name)
         if self.name == "end":
             list_o_paths.append(my_path)
@@ -39,30 +34,24 @@ class Cave:
         return list_o_paths
 
 
-def build_underground(in_str: str) -> Tuple[Cave, Set[str]]:
+def build_underground(in_str: str) -> Cave:
     all_caves = {}
     start = None
     for line in in_str.split("\n"):
         first, second = line.split("-")
         for cave in first, second:
             if cave not in all_caves:
-                all_caves[cave] = Cave(cave)
+                all_caves[cave] = Cave(cave, set(), cave.isupper())
                 if cave == "start":
                     start = all_caves[cave]
         all_caves[first].adjacent.add(all_caves[second])
         all_caves[second].adjacent.add(all_caves[first])
-    small_caves = {
-        cave.name
-        for cave in all_caves.values()
-        if not cave.large and cave.name not in ("start", "end")
-    }
-    return start, small_caves
+    return start
 
 
 if __name__ == "__main__":
     DATA = INPUT_FILE.read_text().strip()
 
-    STARTING_CAVE, SMALL_CAVES = build_underground(DATA)
+    STARTING_CAVE = build_underground(DATA)
     print(len(STARTING_CAVE.navigate_to_end()))
-    P2_PATHS = [STARTING_CAVE.navigate_to_end(dupe_cave=small) for small in SMALL_CAVES]
-    print(len({"->".join(path) for path_attempt in P2_PATHS for path in path_attempt}))
+    print(len(STARTING_CAVE.navigate_to_end(dupe_cave=False)))
