56d2018fe1f115647887fcc5d8d275d7d4bd20fd
Peter Olson <peter.olson@gmail.com>
2021-12-12 22:19:03 -0800

    Cleaned up 2021day13 solution

--- 2021/day13.py
+++ 2021/day13.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import NamedTuple
+from typing import NamedTuple, Tuple, Set, List
 
 INPUT_FILE = Path(__file__).with_suffix(".input")
 
@@ -8,45 +8,40 @@ class Coord(NamedTuple):
     x: int
     y: int
 
-    def __add__(self, other):
-        return Coord(self.x + other.x, self.y + other.y)
-
 
 class Inst(NamedTuple):
     line: str  # x or y
     value: int
 
 
-def parse_input(in_str: str):
+def parse_input(in_str: str) -> Tuple[Set[Coord], List[Inst]]:
     dotstr, inststr = in_str.split("\n\n")
-    dots = {
+    return {
         Coord(int(x), int(y))
         for line in dotstr.split("\n")
         for x, y in [line.split(",")]
-    }
-    insts = [
+    }, [
         Inst(d, int(n))
         for line in inststr.split("\n")
         for d, n in [line.split()[-1].split("=")]
     ]
-    return dots, insts
 
 
-def solve(dots, instructions):
+def flip(loc: int, amount: int) -> int:
+    return amount - (loc - amount)
+
+
+def solve(dots: Set[Coord], instructions: List[Inst]):
     p1 = False
     for inst in instructions:
         if inst.line == "x":
             dots = {
-                Coord(inst.value - (dot.x - inst.value), dot.y)
-                if dot.x > inst.value
-                else dot
+                Coord(flip(dot.x, inst.value), dot.y) if dot.x > inst.value else dot
                 for dot in dots
             }
         elif inst.line == "y":
             dots = {
-                Coord(dot.x, inst.value - (dot.y - inst.value))
-                if dot.y > inst.value
-                else dot
+                Coord(dot.x, flip(dot.y, inst.value)) if dot.y > inst.value else dot
                 for dot in dots
             }
         if not p1:
@@ -56,12 +51,10 @@ def solve(dots, instructions):
 
 
 def print_dots(dots):
-    minx = min(dot.x for dot in dots)
-    maxx = max(dot.x for dot in dots)
-    miny = min(dot.y for dot in dots)
-    maxy = max(dot.y for dot in dots)
+    minx, maxx = min(dot.x for dot in dots), max(dot.x for dot in dots)
+    miny, maxy = min(dot.y for dot in dots), max(dot.y for dot in dots)
     for y in range(miny, maxy + 1):
-        print("".join("#" if (x, y) in dots else "." for x in range(minx, maxx + 1)))
+        print("".join("#" if (x, y) in dots else " " for x in range(minx, maxx + 1)))
 
 
 if __name__ == "__main__":
