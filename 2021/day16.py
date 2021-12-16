049d19b9eff78f94a3494cb274f5b5d3fdb673bc
Peter Olson <peter.olson@gmail.com>
2021-12-15 22:27:45 -0800

    Use fancy format instead of lookup table 2021day16

--- 2021/day16.py
+++ 2021/day16.py
@@ -5,15 +5,6 @@ from typing import List
 
 INPUT_FILE = Path(__file__).with_suffix(".input")
 
-# fmt: off
-HEX_TRANSLATE = {
-    "0": "0000", "1": "0001", "2": "0010", "3": "0011",
-    "4": "0100", "5": "0101", "6": "0110", "7": "0111",
-    "8": "1000", "9": "1001", "A": "1010", "B": "1011",
-    "C": "1100", "D": "1101", "E": "1110", "F": "1111",
-}
-# fmt: on
-
 
 class Packet:
     version: int
@@ -112,6 +103,6 @@ def read_packet(binary: str, start: int = 0) -> Packet:
 if __name__ == "__main__":
     DATA = INPUT_FILE.read_text().strip()
 
-    PACKET = read_packet("".join(HEX_TRANSLATE[c] for c in DATA))
+    PACKET = read_packet(f"{int(DATA, 16):0{len(DATA)*4}b}")
     print(PACKET.sum_ver())
     print(PACKET.total())
