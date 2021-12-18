5b12c3495c8dd07de4429f4795a3bda5c145c7b8
Peter Olson <peter.olson@gmail.com>
2021-12-18 11:38:20 -0800

    Convert from strings to lists 2021day18 (half runtime)

--- 2021/day18.py
+++ 2021/day18.py
@@ -1,104 +1,84 @@
-import re
-from ast import literal_eval
 from itertools import combinations
 from math import ceil, floor
 from pathlib import Path
-from typing import List, Optional
-
-RE_NUMS = re.compile(r"-?\d+")
+from typing import List, Union
 
 INPUT_FILE = Path(__file__).with_suffix(".input")
 
+SnailfishLine = List[Union[str, int]]
 
-def explode(line: str) -> Optional[str]:
+
+def explode(line: SnailfishLine) -> bool:
     depth = 0
-    for i, c in enumerate(line):
-        if c == "[":
+    for i, item in enumerate(line):
+        if item == "[":
             depth += 1
             if depth >= 5:
-                right_bracket = line.find("]", i)
-                to_explode = line[i + 1 : right_bracket]
-                line = line[:i] + "0" + line[right_bracket + 1 :]
-                left_num = right_num = None
-                for match in RE_NUMS.finditer(line):
-                    if match.start() < i:
-                        left_num = match
-                    elif match.start() > i:
-                        right_num = match
+                to_ex_l, to_ex_r = line[i + 1 : i + 3]
+                for j in range(i + 3, len(line)):
+                    if isinstance(line[j], int):
+                        line[j] += to_ex_r
                         break
-                to_ex_l, to_ex_r = map(int, to_explode.split(","))
-                if left_num and right_num:
-                    line = (
-                        line[: left_num.start()]
-                        + f"{to_ex_l + int(left_num.group(0))}"
-                        + line[left_num.end() : right_num.start()]
-                        + f"{to_ex_r + int(right_num.group(0))}"
-                        + line[right_num.end() :]
-                    )
-                elif left_num:
-                    line = (
-                        line[: left_num.start()]
-                        + f"{to_ex_l + int(left_num.group(0))}"
-                        + line[left_num.end() :]
-                    )
-                elif right_num:
-                    line = (
-                        line[: right_num.start()]
-                        + f"{to_ex_r + int(right_num.group(0))}"
-                        + line[right_num.end() :]
-                    )
-                return line
-        elif c == "]":
+                for k in range(i - 1, 0, -1):
+                    if isinstance(line[k], int):
+                        line[k] += to_ex_l
+                        break
+                line[i : i + 4] = [0]
+                return True
+        elif item == "]":
             depth -= 1
 
 
-def split(line: str) -> Optional[str]:
-    for match in RE_NUMS.finditer(line):
-        value = int(match.group(0))
-        if value >= 10:
-            half = value / 2
-            return (
-                line[: match.start()]
-                + f"[{floor(half)},{ceil(half)}]"
-                + line[match.end() :]
-            )
+def split(line: SnailfishLine) -> bool:
+    for i, item in enumerate(line):
+        if isinstance(item, int) and item >= 10:
+            half = item / 2
+            line[i : i + 1] = ["[", floor(half), ceil(half), "]"]
+            return True
 
 
-def reduce(line: str):
+def reduce(line: SnailfishLine):
     while True:
-        if line_exp := explode(line):
-            line = line_exp
+        if explode(line):
             continue
-        if line_split := split(line):
-            line = line_split
+        if split(line):
             continue
         return line
 
 
-def part1(lines: List[str]):
+def part1(lines: List[SnailfishLine]) -> SnailfishLine:
     so_far, *lines = lines
     for line in lines:
-        so_far = reduce(f"[{so_far},{line}]")
+        so_far = reduce(["["] + so_far + line + ["]"])
     return so_far
 
 
-def calc_magnitude(data: str) -> int:
-    if isinstance(data, str):
-        data = literal_eval(data)
-    if isinstance(data, int):
-        return data
-    if isinstance(data, list):
-        return 3 * calc_magnitude(data[0]) + 2 * calc_magnitude(data[1])
+def calc_magnitude(data: SnailfishLine) -> int:
+    nested: List[List[int]] = []
+    for i, item in enumerate(data):
+        if item == "[":
+            nested.append([])
+        elif item == "]":
+            current = nested.pop(-1)
+            if nested:
+                nested[-1].append(3 * current[0] + 2 * current[1])
+            else:
+                return 3 * current[0] + 2 * current[1]
+        else:
+            nested[-1].append(item)
 
 
 if __name__ == "__main__":
     DATA = INPUT_FILE.read_text().strip()
-    SNAILFISH_ARRAY = [line for line in DATA.split("\n")]
+    SNAILFISH_ARRAY = [
+        [int(c) if c.isdigit() else c for c in line if c != ","]
+        for line in DATA.split("\n")
+    ]
 
     print(calc_magnitude(part1(SNAILFISH_ARRAY)))
     print(
         max(
-            calc_magnitude(reduce(f"[{left},{right}]"))
+            calc_magnitude(reduce(["["] + left + right + ["]"]))
             for left, right in combinations(SNAILFISH_ARRAY, 2)
         )
     )
