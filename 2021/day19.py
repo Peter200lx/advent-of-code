97229a8beca1bea57fa7a1d7f35265be1e1a5d33
Peter Olson <peter.olson@gmail.com>
2021-12-19 17:34:34 -0800

    Flip comparison so only modify self not other 2021day19

--- 2021/day19.py
+++ 2021/day19.py
@@ -85,17 +85,15 @@ class Scanner:
             points = {conv.transf.mutate(p) - conv.shift for p in points}
         return points
 
-    def find_relative(
-        self, other: "Scanner", orientation_chain: List[FullConversion]
-    ) -> bool:
-        if not other.transformation_deltas:
-            other.build_transformation_deltas()
+    def find_relative(self, other: "Scanner") -> bool:
+        if not self.transformation_deltas:
+            self.build_transformation_deltas()
         overlapping_deltas = (
-            self.point_deltas.keys() & other.transformation_deltas.keys()
+            other.point_deltas.keys() & self.transformation_deltas.keys()
         )
         if len(overlapping_deltas) >= 12:
             overlap_transform_counter = Counter(
-                other.transformation_deltas[k][0] for k in overlapping_deltas
+                self.transformation_deltas[k][0] for k in overlapping_deltas
             )
             for rel_transf, count in overlap_transform_counter.most_common():
                 if count < 12:
@@ -103,20 +101,20 @@ class Scanner:
                 an_overlap = next(
                     od
                     for od in overlapping_deltas
-                    if other.transformation_deltas[od][0] == rel_transf
+                    if self.transformation_deltas[od][0] == rel_transf
                 )
-                my_p1, my_p2 = self.point_deltas[an_overlap]
-                other_p1, other_p2 = other.point_deltas[
-                    other.transformation_deltas[an_overlap][1]
+                other_p1, other_p2 = other.point_deltas[an_overlap]
+                my_p1, my_p2 = self.point_deltas[
+                    self.transformation_deltas[an_overlap][1]
                 ]
-                point_delta = rel_transf.mutate(other_p1) - my_p1
-                assert rel_transf.mutate(other_p2) - my_p2 == point_delta
+                point_delta = rel_transf.mutate(my_p1) - other_p1
+                assert rel_transf.mutate(my_p2) - other_p2 == point_delta
                 transformed_points = {
-                    rel_transf.mutate(p) - point_delta for p in other.seen_points
+                    rel_transf.mutate(p) - point_delta for p in self.seen_points
                 }
-                if len(set(self.seen_points) & transformed_points) < 12:
+                if len(set(other.seen_points) & transformed_points) < 12:
                     continue
-                other.orientation_from_0 = orientation_chain + [
+                self.orientation_from_0 = other.orientation_from_0 + [
                     FullConversion(rel_transf, point_delta)
                 ]
                 return True
@@ -153,7 +151,7 @@ def solve(scanners: List[Scanner]) -> Tuple[int, int]:
         first = known_scanners.pop()
         to_drop = []
         for i, second in enumerate(unincorporated_scanners):
-            if first.find_relative(second, first.orientation_from_0):
+            if second.find_relative(first):
                 to_drop.append(i)
                 second_points = second.corrected_points()
                 assert len(points & second_points) >= 12
