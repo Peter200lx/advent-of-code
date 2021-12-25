cd48a8a5a9fd1c34723fefa9b008136b51e469a9
Peter Olson <peter.olson@gmail.com>
2021-12-24 22:08:21 -0800

    Slight cleanup for 2021day25

--- 2021/day25.py
+++ 2021/day25.py
@@ -12,7 +12,6 @@ class Coord(NamedTuple):
 def step(
     east_herd: Set[Coord], south_herd: Set[Coord], max_size: Coord
 ) -> Tuple[Set[Coord], Set[Coord]]:
-    assert not east_herd & south_herd, "No points should ever overlap"
     all_points = east_herd | south_herd
     east_herd = {
         Coord(*t) if (t := ((c.x + 1) % max_size.x, c.y)) not in all_points else c
@@ -30,9 +29,8 @@ def solve(east_herd: Set[Coord], south_herd: Set[Coord]) -> int:
     maxx = max(max(c.x for c in south_herd), max(c.x for c in east_herd))
     maxy = max(max(c.y for c in south_herd), max(c.y for c in east_herd))
     max_size = Coord(maxx + 1, maxy + 1)
-    last_east, last_south = east_herd, south_herd
-    east_herd, south_herd = step(east_herd, south_herd, max_size)
-    step_count = 1
+    last_east = last_south = None
+    step_count = 0
     while not (last_south == south_herd and last_east == east_herd):
         last_east, last_south = east_herd, south_herd
         east_herd, south_herd = step(east_herd, south_herd, max_size)
