4a58a0367a4151702623010388827ef305a34ea0
Peter Olson <peter.olson@gmail.com>
2022-11-30 21:11:35 -0800

    Slight cleanup for 2022day01

--- 2022/day01.py
+++ 2022/day01.py
@@ -4,18 +4,17 @@ from typing import List
 INPUT_FILE = Path(__file__).with_suffix(".input")
 
 
-def count_elves(input_list: List[List[int]]) -> List[int]:
-    return [sum(elf) for elf in input_list]
+def count_elves(elf_list: List[List[int]]) -> List[int]:
+    return [sum(elf) for elf in elf_list]
 
 
-def top_n_sum(input_list: List[int], num: int = 3) -> int:
-    input_list.sort(reverse=True)
-    return sum(input_list[:num])
+def top_n_sum(elf_list: List[int], num: int = 3) -> int:
+    return sum(sorted(elf_list, reverse=True)[:num])
 
 
 if __name__ == "__main__":
     DATA = INPUT_FILE.read_text().strip()
-    INPUT_ELVES = [[int(i) for i in l.split("\n")] for l in DATA.split("\n\n")]
+    INPUT_ELVES = [[int(i) for i in elf.split("\n")] for elf in DATA.split("\n\n")]
 
     print(max(count_elves(INPUT_ELVES)))
 
