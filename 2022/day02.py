06dc90d4203d95d29c87af097e61e2701014340d
Peter Olson <peter.olson@gmail.com>
2022-12-01 21:27:27 -0800

    Cleaned up 2022day02

--- 2022/day02.py
+++ 2022/day02.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from typing import List, Tuple
 
 INPUT_FILE = Path(__file__).with_suffix(".input")
 
@@ -11,53 +10,37 @@ TYPE = {
     "Y": 2,
     "Z": 3,
 }
-
 CYCLE = {
     1: 2,
     2: 3,
     3: 1,
 }
-
 LOSS = 0
 DRAW = 3
 WIN = 6
 
 
-def part1(strat: List[Tuple[str, str]]):
-    total = 0
-    for game in strat:
-        opp, me = TYPE[game[0]], TYPE[game[1]]
-        if opp == me:
-            res = DRAW + me
-        elif CYCLE[opp] == me:
-            res = WIN + me
-        else:
-            res = LOSS + me
-        total += res
-    return total
+def part1(c1: str, c2: str) -> int:
+    opp, me = TYPE[c1], TYPE[c2]
+    if opp == me:
+        return DRAW + me
+    elif CYCLE[opp] == me:
+        return WIN + me
+    return LOSS + me
 
 
-def part2(strat: List[Tuple[str, str]]):
-    total = 0
-    for game in strat:
-        opp, me = TYPE[game[0]], TYPE[game[1]]
-        if game[1] == "Y":
-            res = DRAW + opp
-        elif game[1] == "Z":
-            res = WIN + CYCLE[opp]
-        else:
-            res = LOSS + CYCLE[CYCLE[opp]]
-        total += res
-    return total
-
-
-def top_n_sum(e):
-    return e
+def part2(c1: str, c2: str) -> int:
+    opp = TYPE[c1]
+    if c2 == "Y":
+        return DRAW + opp
+    elif c2 == "Z":
+        return WIN + CYCLE[opp]
+    return LOSS + CYCLE[CYCLE[opp]]
 
 
 if __name__ == "__main__":
     DATA = INPUT_FILE.read_text().strip()
-    INPUT_ELVES = [tuple(c for c in line.split()) for line in DATA.split("\n")]
+    INPUT_ELVES = [[c for c in line.split()] for line in DATA.split("\n")]
 
-    print(part1(INPUT_ELVES))
-    print(part2(INPUT_ELVES))
+    print(sum(part1(r[0], r[1]) for r in INPUT_ELVES))
+    print(sum(part2(r[0], r[1]) for r in INPUT_ELVES))
