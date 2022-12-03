88a87df7e39efce6659bf69a8b16e34569d724a4
Peter Olson <peter.olson@gmail.com>
2022-12-02 21:29:17 -0800

    Cleaned up 2022day03 solution

--- 2022/day03.py
+++ 2022/day03.py
@@ -3,21 +3,23 @@ from pathlib import Path
 
 INPUT_FILE = Path(__file__).with_suffix(".input")
 
-VALUE = {c: i for i, c in enumerate(string.ascii_lowercase + string.ascii_uppercase, start=1)}
+VALUE = {
+    c: i + 1 for i, c in enumerate(string.ascii_lowercase + string.ascii_uppercase)
+}
 
 
 def sack_delta(sack: str) -> int:
     assert len(sack) % 2 == 0
-    s1, s2 = sack[:len(sack)//2], sack[len(sack)//2:]
-    return VALUE[next(iter(set(s1) & set(s2)))]
+    return VALUE[(set(sack[: len(sack) // 2]) & set(sack[len(sack) // 2 :])).pop()]
 
 
 def part2(s1: str, s2: str, s3: str) -> int:
-    return VALUE[next(iter(set(s1) & set(s2) & set(s3)))]
+    return VALUE[(set(s1) & set(s2) & set(s3)).pop()]
+
 
 if __name__ == "__main__":
     DATA = INPUT_FILE.read_text().strip()
     INPUT_DATA = [line for line in DATA.split("\n")]
 
     print(sum(sack_delta(r) for r in INPUT_DATA))
-    print(sum(part2(INPUT_DATA[i], INPUT_DATA[i+1], INPUT_DATA[i+2]) for i in range(0, len(INPUT_DATA), 3)))
+    print(sum(part2(*INPUT_DATA[i : i + 3]) for i in range(0, len(INPUT_DATA), 3)))
