34af4ed13245b42fcc53264c8529b02bc8ad0f17
Peter Olson <peter.olson@gmail.com>
2022-12-03 21:45:18 -0800

    Combined solutions for 2022day04

--- 2022/day04.py
+++ 2022/day04.py
@@ -1,19 +1,17 @@
 from pathlib import Path
-from typing import List
+from typing import List, Tuple
 
 INPUT_FILE = Path(__file__).with_suffix(".input")
 
 
-def part1(e1: List[int], e2: List[int]) -> bool:
-    s1, s2 = set(range(e1[0], e1[1] + 1)), set(range(e2[0], e2[1] + 1))
-    if len(s1) > len(s2):
-        return s1 & s2 == s2
-    return s1 & s2 == s1
-
-
-def part2(e1: List[int], e2: List[int]) -> bool:
-    s1, s2 = set(range(e1[0], e1[1] + 1)), set(range(e2[0], e2[1] + 1))
-    return bool(s1 & s2)
+def solve(items: List[List[int]]) -> Tuple[int, int]:
+    p1 = p2 = 0
+    for e1, e2 in items:
+        s1, s2 = set(range(e1[0], e1[1] + 1)), set(range(e2[0], e2[1] + 1))
+        comb = s1 & s2
+        p1 += len(comb) == min(len(s1), len(s2))
+        p2 += 1 if comb else 0
+    return p1, p2
 
 
 if __name__ == "__main__":
@@ -23,5 +21,4 @@ if __name__ == "__main__":
         for line in DATA.split("\n")
     ]
 
-    print(sum(part1(*l) for l in INPUT_DATA))
-    print(sum(part2(*l) for l in INPUT_DATA))
+    print(solve(INPUT_DATA))
