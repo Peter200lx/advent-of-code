cb0ba92e9ba8efd580215091bba439854073147d
Peter Olson <peter.olson@gmail.com>
2022-12-07 22:14:39 -0800

    Vastly clean up 2022day08 part 2

--- 2022/day08.py
+++ 2022/day08.py
@@ -29,54 +29,19 @@ def solve(grid: List[List[int]]) -> int:
     return len(seen_trees)
 
 
-def tree_score(trees: Dict[Tuple[int, int], int], start_x: int, start_y: int) -> int:
+def tree_score(forest: Dict[Tuple[int, int], int], start_x: int, start_y: int) -> int:
     score = 1
-    my_height = trees.get((start_x, start_y))
-
-    seen_trees = 0
-    for i in range(1, 999):
-        tree_up = trees.get((start_x + i, start_y), 999)
-        if tree_up < my_height:
+    my_height = forest.get((start_x, start_y))
+    for direc in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+        seen_trees = 0
+        for i in range(1, 999):
+            next_tree = forest.get((start_x + direc[0] * i, (start_y + direc[1] * i)))
+            if next_tree is None:
+                break
             seen_trees += 1
-        else:
-            if tree_up != 999:
-                seen_trees += 1
-            break
-
-    score *= seen_trees
-    seen_trees = 0
-    for i in range(1, 999):
-        tree_down = trees.get((start_x - i, start_y), 999)
-        if tree_down < my_height:
-            seen_trees += 1
-        else:
-            if tree_down != 999:
-                seen_trees += 1
-            break
-
-    score *= seen_trees
-    seen_trees = 0
-    for i in range(1, 999):
-        tree_left = trees.get((start_x, start_y - i), 999)
-        if tree_left < my_height:
-            seen_trees += 1
-        else:
-            if tree_left != 999:
-                seen_trees += 1
-            break
-
-    score *= seen_trees
-    seen_trees = 0
-    for i in range(1, 999):
-        tree_right = trees.get((start_x, start_y + i), 999)
-        if tree_right < my_height:
-            seen_trees += 1
-        else:
-            if tree_right != 999:
-                seen_trees += 1
-            break
-
-    score *= seen_trees
+            if my_height <= next_tree:
+                break
+        score *= seen_trees
     return score
 
 
@@ -85,11 +50,7 @@ def top_scenic(grid: List[List[int]]) -> int:
     for y, line in enumerate(grid):
         for x, height in enumerate(line):
             tree_point_cloud[(x, y)] = height
-    best_score = 0
-    for x in range(1, len(grid[0]) - 1):
-        for y in range(1, len(grid) - 1):
-            best_score = max(best_score, tree_score(tree_point_cloud, x, y))
-    return best_score
+    return max(tree_score(tree_point_cloud, *point) for point in tree_point_cloud)
 
 
 if __name__ == "__main__":
