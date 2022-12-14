f1a1f700b40beb2a1b69ccf0e859beb335f4a80d
Peter Olson <peter.olson@gmail.com>
2022-12-13 22:41:50 -0800

    Converted 2022day14 part 1 solution from numpy to point cloud

--- 2022/day14.py
+++ 2022/day14.py
@@ -2,16 +2,20 @@ import re
 import sys
 from enum import Enum
 from pathlib import Path
-from typing import NamedTuple, Union
-
-import numpy as np
+from typing import NamedTuple, Union, Dict, List, Optional
 
 INPUT_FILE = Path(__file__).with_suffix(".input")
 
 
 class Slice(NamedTuple):
-    y: Union[slice, int]
-    x: Union[slice, int]
+    y: Union[range, int]
+    x: Union[range, int]
+
+    def gen(self):
+        if isinstance(self.y, int) and isinstance(self.x, range):
+            yield from (Coord(self.y, x) for x in self.x)
+        elif isinstance(self.y, range) and isinstance(self.x, int):
+            yield from (Coord(y, self.x) for y in self.y)
 
 
 class Coord(NamedTuple):
@@ -26,8 +30,7 @@ class Range(NamedTuple):
     x_max: int
 
 
-RE_NUMS = re.compile(r"-?\d+")
-SOURCES = [Coord(y=0, x=500)]
+SOURCE = Coord(y=0, x=500)
 
 
 class FieldTypes(Enum):
@@ -39,17 +42,12 @@ class FieldTypes(Enum):
 
 
 FIELD_PRINT = {
-    FieldTypes.ROCK.value: "#",
-    FieldTypes.EMPTY.value: ".",
-    FieldTypes.SAND_STATIONARY.value: "o",
-    FieldTypes.SAND_FALLING.value: "~",
-    FieldTypes.SOURCE.value: "+",
+    FieldTypes.ROCK: "#",
+    FieldTypes.EMPTY: ".",
+    FieldTypes.SAND_STATIONARY: "o",
+    FieldTypes.SAND_FALLING: "~",
+    FieldTypes.SOURCE: "+",
 }
-
-
-np.set_printoptions(
-    linewidth=500, threshold=sys.maxsize, formatter={"int": lambda x: FIELD_PRINT[x]}
-)
 
 
 def parse_input(data_blob):
@@ -71,7 +69,7 @@ def parse_input(data_blob):
                 data_list.append(
                     (
                         Slice(
-                            y=slice(min(start_y, end_y), max(start_y, end_y) + 1),
+                            y=range(min(start_y, end_y), max(start_y, end_y) + 1),
                             x=end_x,
                         )
                     )
@@ -79,39 +77,47 @@ def parse_input(data_blob):
             elif start_y == end_y:
                 data_list.append(
                     Slice(
-                        y=end_y, x=slice(min(start_x, end_x), max(start_x, end_x) + 1)
+                        y=end_y, x=range(min(start_x, end_x), max(start_x, end_x) + 1)
                     )
                 )
     return data_list, Range(min_y - 1, max_y + 1 + 2, min_x - 1, max_x + 1)
 
 
 class Field:
-    def __init__(self, scan_list, frange, source_locs):
+    def __init__(self, scan_list: List[Slice], frange: Range, source: Coord):
         self.range = frange
-        self.array = np.zeros((frange.y_max, frange.x_max), dtype=np.int8)
+        self.point_cloud: Dict[Coord, FieldTypes] = {}
         for scan in scan_list:
-            self.array[scan] = FieldTypes.ROCK.value
+            for point in scan.gen():
+                self.point_cloud[point] = FieldTypes.ROCK
         self.falling_sand = set()
-        for loc in source_locs:
-            self.array[loc] = FieldTypes.SOURCE.value
-            sand_loc = Coord(loc.y + 1, loc.x)
-            self.falling_sand.add(sand_loc)
-            self.array[sand_loc] = FieldTypes.SAND_FALLING.value
+        self.point_cloud[source] = FieldTypes.SOURCE
+        sand_loc = Coord(source.y + 1, source.x)
+        self.falling_sand.add(sand_loc)
+        self.point_cloud[sand_loc] = FieldTypes.SAND_FALLING
 
     def print(self):
-        print(self.array[:, self.range.x_min - 1 :])
+        for y in range(0, self.range.y_max):
+            print(
+                "".join(
+                    FIELD_PRINT[self.point_cloud[Coord(y, x)]]
+                    if Coord(y, x) in self.point_cloud
+                    else "."
+                    for x in range(self.range.x_min - 1, self.range.x_max + 1)
+                )
+            )
 
-    def loc_type(self, loc):
-        return FieldTypes(self.array[loc])
+    def loc_type(self, loc: Coord) -> FieldTypes:
+        return self.point_cloud.get(loc, FieldTypes.EMPTY)
 
-    def drop_sand(self, loc):
+    def drop_sand(self, loc: Coord) -> Optional[Coord]:
         if loc not in self.falling_sand:
             self.falling_sand.add(loc)
         below_loc = Coord(loc.y + 1, loc.x)
         if below_loc.y == self.range.y_max:
-            return False
+            return
         while self.loc_type(below_loc) == FieldTypes.EMPTY:
-            self.array[below_loc] = FieldTypes.SAND_FALLING.value
+            self.point_cloud[below_loc] = FieldTypes.SAND_FALLING
             self.falling_sand.add(below_loc)
             below_loc = Coord(below_loc.y + 1, below_loc.x)
             if below_loc.y == self.range.y_max:
@@ -122,7 +128,7 @@ class Field:
         point_array = []
         new_point = Coord(y=orig_loc.y + 1, x=orig_loc.x + direc)
         while self.loc_type(new_point) is FieldTypes.EMPTY:
-            self.array[new_point] = FieldTypes.SAND_FALLING.value
+            self.point_cloud[new_point] = FieldTypes.SAND_FALLING
             self.falling_sand.add(new_point)
             below_loc = Coord(new_point.y + 1, new_point.x)
             if below_loc.y == self.range.y_max:
@@ -138,7 +144,7 @@ class Field:
         if self.loc_type(new_point) in (FieldTypes.ROCK, FieldTypes.SAND_STATIONARY):
             for p in point_array:
                 self.falling_sand.discard(p)
-                self.array[p] = FieldTypes.SAND_STATIONARY.value
+                self.point_cloud[p] = FieldTypes.SAND_STATIONARY
             return True
 
     def pile(self, loc: Coord):
@@ -146,7 +152,7 @@ class Field:
         if left_stationary:
             right_stationary = self.fill_angle(loc, direc=+1)
             if right_stationary:
-                self.array[loc] = FieldTypes.SAND_STATIONARY.value
+                self.point_cloud[loc] = FieldTypes.SAND_STATIONARY
 
     def step_sand(self):
         old_set = self.falling_sand.copy()
@@ -168,7 +174,7 @@ def run_simulation(field):
     while field.step_sand():
         pass
     field.print()
-    return (field.array == FieldTypes.SAND_STATIONARY.value).sum()
+    return sum(v == FieldTypes.SAND_STATIONARY for v in field.point_cloud.values())
 
 
 if __name__ == "__main__":
@@ -177,5 +183,5 @@ if __name__ == "__main__":
 503,4 -> 502,4 -> 502,9 -> 494,9"""
 
     SCAN_OUTPUT, RANGE = parse_input(DATA)
-    GROUND = Field(SCAN_OUTPUT, RANGE, SOURCES)
+    GROUND = Field(SCAN_OUTPUT, RANGE, SOURCE)
     print(run_simulation(GROUND))
