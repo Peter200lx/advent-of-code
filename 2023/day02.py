ef70325953a75d713e1c317509af3c2997e23d66
Peter Olson <peter.olson@gmail.com>
2023-12-01 21:34:29 -0800

    Clean up and type 2023day02 solution

--- 2023/day02.py
+++ 2023/day02.py
@@ -1,4 +1,6 @@
+from math import prod
 from pathlib import Path
+from typing import Dict
 
 INPUT_FILE = Path(__file__).with_suffix(".input")
 
@@ -8,47 +10,28 @@ PART_1 = {"red": 12, "green": 13, "blue": 14}
 class Game:
     def __init__(self, line: str):
         game, rounds = line.split(": ")
-        rounds = rounds.split("; ")
         self.id = int(game.split()[1])
         self.rounds = [
             {name: int(n) for part in r.split(", ") for n, name in [part.split()]}
-            for r in rounds
+            for r in rounds.split("; ")
         ]
 
-    def possible(self, total):
-        for round in self.rounds:
-            for color, n in total.items():
-                if round.get(color, 0) > n:
-                    return False
-        return True
+    def possible(self, req: Dict[str, int]) -> bool:
+        return not any(
+            turn.get(color, 0) > n for color, n in req.items() for turn in self.rounds
+        )
 
-    def min(self):
+    def req(self) -> Dict[str, int]:
         totals = {"red": 0, "green": 0, "blue": 0}
-        for round in self.rounds:
+        for turn in self.rounds:
             for color in totals:
-                totals[color] = max(totals[color], round.get(color, 0))
+                totals[color] = max(totals[color], turn.get(color, 0))
         return totals
-
-
-def part_one(games):
-    nums = 0
-    for game in games:
-        if game.possible(PART_1):
-            nums += game.id
-    return nums
-
-
-def part_two(games):
-    powers = 0
-    for game in games:
-        req = game.min()
-        powers += req.get("red") * req.get("green") * req.get("blue")
-    return powers
 
 
 if __name__ == "__main__":
     DATA = INPUT_FILE.read_text().strip()
     INPUT = [Game(line) for line in DATA.split("\n")]
 
-    print(part_one(INPUT))
-    print(part_two(INPUT))
+    print(sum(game.id for game in INPUT if game.possible(PART_1)))
+    print(sum(prod(game.req().values()) for game in INPUT))
