a74853db34cb6a9ac2290b35a943d7c846fdebc3
Peter Olson <peter.olson@gmail.com>
2023-12-08 21:47:52 -0800

    2023day09 use reversed list from Caleb's solution
    https://github.com/calebjcourtney/advent-of-code/blob/main/2023/python/day09.py

--- 2023/day09.py
+++ 2023/day09.py
@@ -4,18 +4,13 @@ from typing import List
 INPUT_FILE = Path(__file__).with_suffix(".input")
 
 
-def solve_line(nums: List[int], p1: bool = True) -> int:
+def solve_line(nums: List[int]) -> int:
     deltas = []
-    for i, n in enumerate(nums):
-        if i == 0:
-            continue
-        deltas.append(n - nums[i - 1])
+    for i in range(1, len(nums)):
+        deltas.append(nums[i] - nums[i - 1])
     if all(n == 0 for n in deltas):
         return 0
-    if p1:
-        return deltas[-1] + solve_line(deltas, p1)
-    else:
-        return deltas[0] - solve_line(deltas, p1)
+    return deltas[-1] + solve_line(deltas)
 
 
 if __name__ == "__main__":
@@ -23,4 +18,4 @@ if __name__ == "__main__":
     LINES = [[int(n) for n in line.split()] for line in DATA.splitlines()]
 
     print(sum(line[-1] + solve_line(line) for line in LINES))
-    print(sum(line[0] - solve_line(line, p1=False) for line in LINES))
+    print(sum(line[0] + solve_line(line[::-1]) for line in LINES))
