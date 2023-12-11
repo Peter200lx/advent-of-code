9d77589867af0e8dd1d00cd0d5a3397e2e9bd99d
Peter Olson <peter.olson@gmail.com>
2023-12-10 21:49:50 -0800

    Simplify 2023day11 with known behaviors

--- 2023/day11.py
+++ 2023/day11.py
@@ -20,12 +20,11 @@ def load_galaxies(instr: str) -> Tuple[Set[Coord], Set[Coord]]:
         if c == "#"
     }
     gal_x, gal_y = {c.x for c in galaxies}, {c.y for c in galaxies}
-    minx, maxx = min(c.x for c in galaxies), max(c.x for c in galaxies)
-    miny, maxy = min(c.y for c in galaxies), max(c.y for c in galaxies)
+    minx, maxx = min(n for n in gal_x), max(n for n in gal_x)
+    miny, maxy = min(n for n in gal_y), max(n for n in gal_y)
     empty_columns = {n for n in range(minx, maxx + 1) if n not in gal_x}
     empty_rows = {n for n in range(miny, maxy + 1) if n not in gal_y}
-    p1_gal = set()
-    p2_gal = set()
+    p1_gal, p2_gal = set(), set()
     p1_row_add = p2_row_add = 0
     for y in range(miny, maxy + 1):
         if y in empty_rows:
@@ -45,11 +44,10 @@ def load_galaxies(instr: str) -> Tuple[Set[Coord], Set[Coord]]:
 
 
 def solve(galaxies: Set[Coord]) -> int:
-    processed = set(galaxies)
     manhs = 0
-    while processed:
-        next_gal = processed.pop()
-        for other_gal in processed:
+    while galaxies:
+        next_gal = galaxies.pop()
+        for other_gal in galaxies:
             manhs += next_gal.manh(other_gal)
     return manhs
 
