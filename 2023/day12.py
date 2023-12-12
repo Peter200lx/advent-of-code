27584d45a8f9562c6c1f2ccdf220fabf26a00c65
Peter Olson <peter.olson@gmail.com>
2023-12-11 23:23:26 -0800

    Combine parts & simplify 2023day12

--- 2023/day12.py
+++ 2023/day12.py
@@ -1,6 +1,6 @@
 from functools import lru_cache
 from pathlib import Path
-from typing import Iterable, List, Tuple, Optional
+from typing import Iterable, Tuple, Optional
 
 INPUT_FILE = Path(__file__).with_suffix(".input")
 
@@ -19,27 +19,25 @@ def test_possible(unknown, index: int, springs: Iterable[bool]):
 def recurse_tests(
     unknown: Tuple[Optional[bool], ...], index: int, known_broken: Tuple[int, ...]
 ) -> int:
-    potential_range = sum(known_broken) + len(known_broken) - 1
+    max_index = len(unknown) - (sum(known_broken) + len(known_broken) - 1)
     if index:  # Only add the non-broken spring if we're not in the first segment
         separator = (False,)
-        max_index = len(unknown) - potential_range
     else:
         separator = tuple()
-        max_index = len(unknown) - potential_range + 1
+        max_index += 1
     working = 0
     for i in range(index, max_index):
-        cur_broken, *remaining_broken = known_broken
-        try_list = separator + ((False,) * (i - index)) + ((True,) * cur_broken)
-        new_index = index + len(try_list)
+        try_list = separator + ((False,) * (i - index)) + ((True,) * known_broken[0])
         if not test_possible(unknown, index, try_list):
             continue
-        if remaining_broken:
-            working += recurse_tests(unknown, new_index, tuple(remaining_broken))
+        if len(known_broken) > 1:
+            working += recurse_tests(unknown, index + len(try_list), known_broken[1:])
         else:
-            try_list += (False,) * (len(unknown) - (index + len(try_list)))
-            if test_possible(unknown, index, try_list):
-                assert len(try_list) + index == len(unknown)
-                working += 1
+            to_add = len(unknown) - (index + len(try_list))
+            if to_add:
+                if not test_possible(unknown, index + len(try_list), (False,) * to_add):
+                    continue
+            working += 1
     return working
 
 
@@ -58,49 +56,17 @@ class Spring:
         self.broken = self.broken[1:]  # Remove the separator from the start
         self.known_broken = self.known_broken * 5
 
-    def test_possible(self, springs: List[bool]):
-        assert len(springs) <= len(self.broken)
-        for i, item in enumerate(springs):
-            if self.broken[i] is not None and item != self.broken[i]:
-                return False
-        return True
-
-    def recurse_tests(self, cur_list: List[bool], known_broken: List[int]) -> List[List[bool]]:
-        length = len(self.broken)
-        potential_range = sum(known_broken) + len(known_broken) - 1
-        working_lists = []
-        start_index = 0 if not cur_list else len(cur_list) + 1
-        for i in range(start_index, length - potential_range + 1):
-            cur_broken, *remaining_broken = known_broken
-            try_list = cur_list + ([False] * (i - len(cur_list))) + ([True] * cur_broken)
-            # print(try_list)
-            if not self.test_possible(try_list):
-                continue
-            if remaining_broken:
-                working_lists.extend(self.recurse_tests(try_list, remaining_broken))
-            else:
-                try_list += [False] * (length - len(try_list))
-                if self.test_possible(try_list):
-                    assert len(try_list) == length
-                    working_lists.append(try_list)
-        return working_lists
-
     def possible_maps(self):
-        working_answers = self.recurse_tests([], self.known_broken)
-        return working_answers
-
-    def num_optimized_possible_maps(self):
-        working_answers = recurse_tests(tuple(self.broken), 0, tuple(self.known_broken))
-        return working_answers
+        return recurse_tests(tuple(self.broken), 0, tuple(self.known_broken))
 
 
 if __name__ == "__main__":
     DATA = INPUT_FILE.read_text().strip()
     SPRINGS = [Spring(line) for line in DATA.split("\n")]
 
-    print(sum(len(s.possible_maps()) for s in SPRINGS))
+    print(sum(s.possible_maps() for s in SPRINGS))
 
     for s in SPRINGS:
         s.times_five()
 
-    print(sum(s.num_optimized_possible_maps() for s in SPRINGS))
+    print(sum(s.possible_maps() for s in SPRINGS))
