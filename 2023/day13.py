bb0c552896227afd919bad695d45f1911ebc1426
Peter Olson <peter.olson@gmail.com>
2023-12-12 22:16:19 -0800

    Collapse 2023day13 into a single calc and pass orientations in

--- 2023/day13.py
+++ 2023/day13.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import NamedTuple, Optional
+from typing import List, NamedTuple, Optional, Set
 
 INPUT_FILE = Path(__file__).with_suffix(".input")
 
@@ -18,73 +18,55 @@ class Pattern:
         self.max = Coord(len(lines[0]), len(lines))
         self.matched_line = set()
 
-    def calc_ref(self) -> int:
+    def part_one(self) -> int:
         return self.calc_set(self.rocks)
 
     def calc_smudge(self) -> int:
         for y in range(self.max.y):
             for x in range(self.max.x):
                 if (x, y) in self.rocks:
-                    rocks = set(self.rocks)
-                    rocks.discard((x, y))
+                    result = self.calc_set(self.rocks - {(x, y)})
                 else:
-                    rocks = self.rocks | {Coord(x, y)}
-                result = self.calc_set(rocks)
+                    result = self.calc_set(self.rocks | {Coord(x, y)})
                 if result is not None:
                     return result
 
-    def calc_set(self, rocks) -> Optional[int]:
-        col_values = []
-        possible_mirror_points = []
-        for col in range(self.max.x):
-            col_values.append(sorted(p.y for p in rocks if p.x == col))
-            if len(col_values) > 1:
-                if col_values[col] == col_values[col - 1]:
-                    possible_mirror_points.append(col)
-        for x in possible_mirror_points:
+    def calc_slices(self, key: str, slices: List[List[int]], max_n: int) -> Optional[int]:
+        for n in range(1, max_n):
             failed = False
-            for i in range(self.max.x):
-                my_coord = x + i
-                other_coord = x - 1 - i
-                if 0 <= other_coord and my_coord < self.max.x:
-                    if col_values[my_coord] != col_values[other_coord]:
+            for i in range(max_n):
+                my_coord = n + i
+                other_coord = n - 1 - i
+                if 0 <= other_coord and my_coord < max_n:
+                    if slices[my_coord] != slices[other_coord]:
                         failed = True
                         break
+                else:
+                    break
             if not failed:
-                key = ("C", x)
-                if key in self.matched_line:
+                my_key = (key, n)
+                if my_key in self.matched_line:
                     continue
-                self.matched_line.add(key)
-                return x
-        row_values = []
-        possible_mirror_points = []
-        for row in range(self.max.y):
-            row_values.append(sorted(p.x for p in rocks if p.y == row))
-            if len(row_values) > 1:
-                if row_values[row] == row_values[row - 1]:
-                    possible_mirror_points.append(row)
-        for y in possible_mirror_points:
-            failed = False
-            for i in range(self.max.y + 1):
-                my_coord = y + i
-                other_coord = y - 1 - i
-                if 0 <= other_coord and my_coord < self.max.y:
-                    if row_values[my_coord] != row_values[other_coord]:
-                        failed = True
-                        break
-            if not failed:
-                key = ("R", y)
-                if key in self.matched_line:
-                    continue
-                self.matched_line.add(key)
-                return 100 * y
+                self.matched_line.add(my_key)
+                return n
         return None
+
+    def calc_set(self, rocks: Set[Coord]) -> Optional[int]:
+        col_values = [sorted(p.y for p in rocks if p.x == col) for col in range(self.max.x)]
+        ret = self.calc_slices("C", col_values, self.max.x)
+        if ret is not None:
+            return ret
+        row_values = [sorted(p.x for p in rocks if p.y == row) for row in range(self.max.y)]
+        ret = self.calc_slices("R", row_values, self.max.y)
+        if ret is not None:
+            return 100 * ret
+        return ret
 
 
 if __name__ == "__main__":
     DATA = INPUT_FILE.read_text().strip()
     PATTERNS = [Pattern(chunk) for chunk in DATA.split("\n\n")]
 
-    print(sum(p.calc_ref() for p in PATTERNS))
+    print(sum(p.part_one() for p in PATTERNS))
 
     print(sum(p.calc_smudge() for p in PATTERNS))
