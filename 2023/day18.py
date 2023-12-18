518c8ea994a66c5aae9356ac1480f5c943286702
Peter Olson <peter.olson@gmail.com>
2023-12-17 22:01:36 -0800

    Rework part 1 to use p2 solution 2023day18

--- 2023/day18.py
+++ 2023/day18.py
@@ -41,64 +41,31 @@ HEX_DIRS = {
 class Dig:
     def __init__(self, insts: List[Tuple[str, int, str]]):
         cur = p2_cur = Coord(0, 0)
-        self.walls = {}
+        self.p1_walls = [cur]
         self.p2_walls = [p2_cur]
         for dir_str, length, color in insts:
-            direct = DIRS[dir_str]
-            for i in range(length):
-                cur += direct
-                self.walls[cur] = color
+            cur += DIRS[dir_str] * length
+            self.p1_walls.append(cur)
             p2_length = int(color[:5], 16)
             p2_cur += HEX_DIRS[color[5]] * p2_length
             self.p2_walls.append(p2_cur)
 
-    def print(self):
-        minx, maxx = min(c.x for c in self.walls), max(c.x for c in self.walls)
-        miny, maxy = min(c.y for c in self.walls), max(c.y for c in self.walls)
-        for y in range(miny, maxy + 1):
-            print(
-                "".join(
-                    self.walls[(x, y)][0] if (x, y) in self.walls else "."
-                    for x in range(minx, maxx + 1)
-                )
-            )
-
-    def dug_amount(self):
-        minx = min(c.x for c in self.walls)
-        for point in (c for c in self.walls if c.x == minx):
-            maybe_start = point + DIRS["R"]
-            if maybe_start in self.walls:
-                continue
-            else:
-                break
-        dug = set()
-        to_proc = [maybe_start]
-        while to_proc:
-            cur = to_proc.pop()
-            for adj in DIRS.values():
-                new_cur = cur + adj
-                if new_cur in self.walls or new_cur in dug:
-                    continue
-                dug.add(new_cur)
-                if new_cur not in to_proc:
-                    to_proc.append(new_cur)
-        return len(self.walls) + len(dug)
-
-    def p2_dug_amount(self):
+    def total_dug(self, p2: bool = False):
         # https://www.101computing.net/the-shoelace-algorithm/
+        walls = self.p2_walls if p2 else self.p1_walls
         lace1 = lace2 = 0
-        walls = 0
-        for i in range(1, len(self.p2_walls)):
-            lace1 += self.p2_walls[i - 1].x * self.p2_walls[i].y
-            lace2 += self.p2_walls[i - 1].y * self.p2_walls[i].x
-            if self.p2_walls[i - 1].x == self.p2_walls[i].x:
-                walls += abs(self.p2_walls[i - 1].y - self.p2_walls[i].y)
+        wall_count = 0
+        for i in range(1, len(walls)):
+            lace1 += walls[i - 1].x * walls[i].y
+            lace2 += walls[i - 1].y * walls[i].x
+            if walls[i - 1].x == walls[i].x:
+                wall_count += abs(walls[i - 1].y - walls[i].y)
             else:
-                walls += abs(self.p2_walls[i - 1].x - self.p2_walls[i].x)
-        lace1 += self.p2_walls[-1].x * self.p2_walls[0].y
-        lace2 += self.p2_walls[-1].y * self.p2_walls[0].x
+                wall_count += abs(walls[i - 1].x - walls[i].x)
+        lace1 += walls[-1].x * walls[0].y
+        lace2 += walls[-1].y * walls[0].x
 
-        return abs(lace1 - lace2) // 2 + walls // 2 + 1
+        return abs(lace1 - lace2) // 2 + wall_count // 2 + 1
 
 
 if __name__ == "__main__":
@@ -109,6 +76,6 @@ if __name__ == "__main__":
 
     DIGSITE = Dig(DIGS_INSTS)
 
-    print(DIGSITE.dug_amount())
+    print(DIGSITE.total_dug())
 
-    print(DIGSITE.p2_dug_amount())
+    print(DIGSITE.total_dug(p2=True))
