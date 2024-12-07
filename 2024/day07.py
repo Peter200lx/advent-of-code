4fc99fb18f73fca83c099f86d8f227d0f12dbc6f
Peter Olson <peter.olson@gmail.com>
2024-12-06 21:54:53 -0800

    Shave 2 sec by early bail 2024day07
    Also run black

--- 2024/day07.py
+++ 2024/day07.py
@@ -4,10 +4,12 @@ from pathlib import Path
 INPUT_FILE = Path(__file__).with_suffix(".input")
 
 
-def is_valid(test: int, vals: list[int], ops="+*") -> bool:
-    for seq in product(ops, repeat=len(vals)-1):
+def is_valid(test: int, vals: list[int], ops="*+") -> bool:
+    for seq in product(ops, repeat=len(vals) - 1):
         result = vals[0]
         for i, c in enumerate(seq, start=1):
+            if result > test:
+                break
             if c == "*":
                 result *= vals[i]
             elif c == "+":
@@ -21,7 +23,11 @@ def is_valid(test: int, vals: list[int], ops="+*") -> bool:
 
 if __name__ == "__main__":
     DATA = INPUT_FILE.read_text().strip()
-    OPERATORS = [(int(left), [int(n) for n in right.split()]) for line in DATA.split("\n") for left, right in [line.split(": ")]]
+    OPERATORS = [
+        (int(left), [int(n) for n in right.split()])
+        for line in DATA.split("\n")
+        for left, right in [line.split(": ")]
+    ]
 
     print(sum(l[0] for l in OPERATORS if is_valid(*l)))
-    print(sum(l[0] for l in OPERATORS if is_valid(*l, ops="+*|")))
+    print(sum(l[0] for l in OPERATORS if is_valid(*l, ops="*+|")))
