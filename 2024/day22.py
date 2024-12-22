9704bc69a60102e75e2ffc43bacec8ba180a8164
Peter Olson <peter.olson@gmail.com>
2024-12-22 00:15:28 -0800

    Only store key tuple 2024day22

--- 2024/day22.py
+++ 2024/day22.py
@@ -3,33 +3,28 @@ from pathlib import Path
 INPUT_FILE = Path(__file__).with_suffix(".input")
 
 
-PRUNE_NUM = 16777216
-
-
 def next_sec(cur: int):
     cur ^= cur * 64
-    cur %= PRUNE_NUM
+    cur %= 16777216
     cur ^= cur // 32
-    cur %= PRUNE_NUM
+    cur %= 16777216
     cur ^= cur * 2048
-    cur %= PRUNE_NUM
+    cur %= 16777216
     return cur
 
 
 def solve(gens: list[int]) -> tuple[int, int]:
     part1 = 0
     seq_values = {}
-    for secret in gens:
-        cur_val = secret
+    for cur_val in gens:
         last_dig = cur_val % 10
-        deltas = []
+        key = tuple()
         seen = set()
         for i in range(2000):
             cur_val = next_sec(cur_val)
             next_last_dig = cur_val % 10
-            deltas.append(next_last_dig - last_dig)
-            if len(deltas) >= 4:
-                key = tuple(deltas[-4:])
+            key = key[-3:] + (next_last_dig - last_dig,)
+            if len(key) >= 4:
                 if key not in seen:
                     if key in seq_values:
                         seq_values[key] += next_last_dig
