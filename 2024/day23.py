70925a58fae93f0dd7d428e606156886caa03ffc
Peter Olson <peter.olson@gmail.com>
2024-12-22 21:55:41 -0800

    Initial 2024day13 solution

---
+++ 2024/day23.py
@@ -0,0 +1,59 @@
+from itertools import combinations
+from pathlib import Path
+
+INPUT_FILE = Path(__file__).with_suffix(".input")
+
+
+class Comp:
+    def __init__(self, name):
+        self.name = name
+        self.connections = set()
+
+
+def build(lines: list[str]):
+    computers = {}
+    for line in lines:
+        first, second = line.split("-")
+        if first not in computers:
+            computers[first] = Comp(first)
+        if second not in computers:
+            computers[second] = Comp(second)
+        computers[first].connections.add(computers[second])
+        computers[second].connections.add(computers[first])
+    return computers
+
+
+def part1(computers: dict[str, Comp]) -> set[frozenset[[Comp]]]:
+    sets = set()
+    for comp in (c for n, c in computers.items() if n.startswith("t")):
+        for a, b in combinations(comp.connections, 2):
+            if a in b.connections:
+                sets.add(frozenset((comp, a, b)))
+    return sets
+
+
+def part2(computers: dict[str, Comp], triples: set[frozenset[[Comp]]]) -> str:
+    big_sets = []
+    for tri_group in triples:
+        a, b, c = list(tri_group)
+        a_set = {n.name for n in a.connections} | {a.name}
+        b_set = {n.name for n in b.connections} | {b.name}
+        c_set = {n.name for n in c.connections} | {c.name}
+        big_sets.append(a_set & b_set & c_set)
+    for valid_set in sorted(big_sets, key=lambda x: len(x), reverse=True):
+        if any(
+            computers[a] not in computers[b].connections
+            for a, b in combinations(valid_set, 2)
+        ):
+            continue
+        return ",".join(c for c in sorted(valid_set))
+
+
+if __name__ == "__main__":
+    DATA = INPUT_FILE.read_text().strip()
+    LINES = list(DATA.split("\n"))
+    COMPUTERS = build(LINES)
+
+    THREE_PAIRS = part1(COMPUTERS)
+    print(len(THREE_PAIRS))
+    print(part2(COMPUTERS, THREE_PAIRS))
