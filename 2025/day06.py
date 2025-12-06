6ebadc3cc923f19ffceca3b48959015f1f18548e
Peter Olson <peter.olson@gmail.com>
2025-12-05 21:52:57 -0800

    Initial 2025day06 solution

---
+++ 2025/day06.py
@@ -0,0 +1,46 @@
+from math import prod
+from pathlib import Path
+
+INPUT_FILE = Path(__file__).with_suffix(".input")
+
+
+def parse(lines: list[str]) -> list[list[str]]:
+    space_index = []
+    for i in range(max(len(line) for line in lines)):
+        if all(line[i] == " " for line in lines):
+            space_index.append(i)
+    space_index.append(None)
+    problems = []
+    last_index = 0
+    for space in space_index:
+        numbers = []
+        for i in range(len(lines) - 1):
+            numbers.append(lines[i][last_index:space])
+        problems.append(numbers + [lines[-1][last_index:space].strip()])
+        if space:
+            last_index = space + 1
+    return problems
+
+
+def part1(problems: list[list[str]]) -> int:
+    return sum(
+        prod(int(n) for n in p[:-1]) if p[-1] == "*" else sum(int(n) for n in p[:-1])
+        for p in problems
+    )
+
+
+def part2(problems: list[list[str]]) -> int:
+    result = 0
+    for prob in problems:
+        nums = [int("".join(n[i] for n in prob[:-1])) for i in range(len(prob[0]))]
+        result += prod(nums) if prob[-1] == "*" else sum(nums)
+    return result
+
+
+if __name__ == "__main__":
+    DATA = INPUT_FILE.read_text().strip()
+
+    PARSED = parse(DATA.split("\n"))
+
+    print(part1(PARSED))
+    print(part2(PARSED))
