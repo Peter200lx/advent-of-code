45ffe5c52d82f5414f3b06624eaffd9422000fa8
Peter Olson <peter.olson@gmail.com>
2025-12-08 23:56:08 -0800

    Initial 2025day09 solution

---
+++ 2025/day09.py
@@ -0,0 +1,59 @@
+from itertools import combinations
+from pathlib import Path
+from typing import NamedTuple
+
+INPUT_FILE = Path(__file__).with_suffix(".input")
+
+
+class Coord(NamedTuple):
+    x: int
+    y: int
+
+
+def p1(field: list[Coord]) -> int:
+    return max(
+        (abs(first.x - second.x) + 1) * (abs(first.y - second.y) + 1)
+        for first, second in combinations(field, 2)
+    )
+
+
+def p2(field: list[Coord]) -> int:
+    lines = []
+    prev_point = field[-1]
+    for point in field:
+        if prev_point.x == point.x:
+            lines.append((point.x, tuple(sorted((point.y, prev_point.y)))))
+        elif prev_point.y == point.y:
+            lines.append((tuple(sorted((point.x, prev_point.x))), point.y))
+        prev_point = point
+    max_size = 0
+    for first, second in combinations(field, 2):
+        min_x, max_x = sorted((first.x, second.x))
+        min_y, max_y = sorted((first.y, second.y))
+        for line in lines:
+            if isinstance(line[0], int):  # Vertical line
+                if min_x < line[0] < max_x and (
+                    line[1][0] <= min_y < line[1][1] or line[1][0] < max_y <= line[1][1]
+                ):
+                    break
+            elif isinstance(line[1], int):  # Horizontal line
+                if (
+                    line[0][0] <= min_x < line[0][1] or line[0][0] < max_x <= line[0][1]
+                ) and min_y < line[1] < max_y:
+                    break
+        else:
+            size = (abs(first.x - second.x) + 1) * (abs(first.y - second.y) + 1)
+            if size >= max_size:
+                # print(f"{size=} {first=} {second=}")
+                max_size = size
+    return max_size
+
+
+if __name__ == "__main__":
+    DATA = INPUT_FILE.read_text().strip()
+    FIELD = [
+        Coord(int(x), int(y)) for line in DATA.split("\n") for x, y in [line.split(",")]
+    ]
+
+    print(p1(FIELD))
+    print(p2(FIELD))
