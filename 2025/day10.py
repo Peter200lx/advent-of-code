fb5c73aae235641303b60afdba09d153e3b5d63b
Peter Olson <peter.olson@gmail.com>
2025-12-13 23:33:27 -0800

    Nearly double 2025day10 part 1 by using dequeue
    We don't need to sort the queue given we're always appending 1
    higher cost.

--- 2025/day10.py
+++ 2025/day10.py
@@ -1,4 +1,4 @@
-import heapq
+from collections import deque
 from pathlib import Path
 from typing import NamedTuple
 
@@ -25,11 +25,10 @@ class Machine(NamedTuple):
         start = 0
         target = sum(1 << i for i, lit in enumerate(self.indc_goal) if lit)
         buttons = [sum(1 << i for i in button) for button in self.buttons]
-        queue = [(1, i, start, set()) for i in range(len(self.buttons))]
-        heapq.heapify(queue)
+        queue = deque((1, i, start, set()) for i in range(len(self.buttons)))
         seen = {start: 0}
         while queue:
-            cost, next_butt, prev_lights, prev_butts = heapq.heappop(queue)
+            cost, next_butt, prev_lights, prev_butts = queue.popleft()
             next_lights = prev_lights ^ buttons[next_butt]
             if next_lights == target:
                 return cost
@@ -40,31 +39,7 @@ class Machine(NamedTuple):
             for i in range(len(buttons)):
                 if i in prev_butts:
                     continue
-                heapq.heappush(queue, (cost + 1, i, next_lights, prev_butts))
-
-    def part2(self) -> int:
-        start = tuple([0] * len(self.joltage))
-        queue = [(1, -99e9, i, start) for i in range(len(self.buttons))]
-        heapq.heapify(queue)
-        seen = {start: 0}
-        while queue:
-            cost, _dist, next_butt, prev_j = heapq.heappop(queue)
-            next_j = tuple(
-                j + 1 if i in self.buttons[next_butt] else j
-                for i, j in enumerate(prev_j)
-            )
-            if seen.get(next_j, 999e9) < cost:
-                continue
-            seen[next_j] = cost
-            if next_j == self.joltage:
-                print(f"{self=} {cost=}")
-                return cost
-            if any(j > self.joltage[i] for i, j in enumerate(next_j)):
-                # print(f"{cost=} {next_j=} {self.joltage=}")
-                continue
-            dist = sum(self.joltage[i] - n for i, n in enumerate(next_j))
-            for i in range(len(self.buttons)):
-                heapq.heappush(queue, (cost + 1, dist, i, next_j))
+                queue.append((cost + 1, i, next_lights, prev_butts))
 
     def part2_scipy(self) -> int:
         A = np.array(
@@ -88,5 +63,4 @@ if __name__ == "__main__":
     MACHINES = [Machine.from_line(line) for line in DATA.split("\n")]
 
     print(sum(m.part1() for m in MACHINES))
-    # print(sum(m.part2() for m in MACHINES))
     print(sum(m.part2_scipy() for m in MACHINES))
